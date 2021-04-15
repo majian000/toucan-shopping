5cd388f3e435e7b07225c7bd88d32b91c50074ba
majian <mmdrss@163.com>
2021-04-15 21:27:14 +0800

    完善配置

--- toucan-shopping-standard/apps/toucan-shopping-web-jar/src/main/java/com/toucan/shopping/standard/apps/web/app/StandardWebApplication.java
+++ toucan-shopping-standard/apps/toucan-shopping-web-jar/src/main/java/com/toucan/shopping/standard/apps/web/app/StandardWebApplication.java
@@ -9,7 +9,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
-@MapperScan({"com.toucan.shopping"})
+@MapperScan({"com.toucan.shopping.modules.common.persistence.event.mapper",
+                "com.toucan.shopping.modules.admin.auth.mapper",
+                "com.toucan.shopping.modules.area.mapper",
+                "com.toucan.shopping.modules.category.mapper",
+                "com.toucan.shopping.modules.column.mapper",
+                "com.toucan.shopping.modules.order.mapper",
+                "com.toucan.shopping.modules.product.mapper",
+                "com.toucan.shopping.modules.stock.mapper",
+                "com.toucan.shopping.modules.user.mapper"})
 @ComponentScan("com.toucan.shopping")
 public class StandardWebApplication {
 
