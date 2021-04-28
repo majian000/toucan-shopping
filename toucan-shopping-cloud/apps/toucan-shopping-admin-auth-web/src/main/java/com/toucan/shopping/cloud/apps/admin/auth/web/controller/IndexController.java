84e5523bff5b21d6ec4df46397e7a5ffdd65b918
majian <mmdrss@163.com>
2021-04-28 11:47:46 +0800

    标准版 集成权限管理

--- toucan-shopping-cloud/apps/toucan-shopping-admin-auth-web/src/main/java/com/toucan/shopping/cloud/apps/admin/auth/web/controller/IndexController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-admin-auth-web/src/main/java/com/toucan/shopping/cloud/apps/admin/auth/web/controller/IndexController.java
@@ -9,6 +9,7 @@ import com.toucan.shopping.cloud.admin.auth.api.feign.service.FeignAdminService;
 import com.toucan.shopping.cloud.admin.auth.api.feign.service.FeignFunctionService;
 import com.toucan.shopping.modules.admin.auth.entity.Admin;
 import com.toucan.shopping.modules.admin.auth.entity.AdminApp;
+import com.toucan.shopping.modules.admin.auth.entity.Function;
 import com.toucan.shopping.modules.admin.auth.vo.AdminVO;
 import com.toucan.shopping.modules.admin.auth.vo.FunctionVO;
 import com.toucan.shopping.modules.auth.admin.AdminAuth;
@@ -86,8 +87,40 @@ public class IndexController {
 
     @AdminAuth(verifyMethod = AdminAuth.VERIFYMETHOD_ADMIN_AUTH,requestType = AdminAuth.REQUEST_FORM)
     @RequestMapping(value = "/welcome",method = RequestMethod.GET)
-    public String welcome()
+    public String welcome(HttpServletRequest request)
     {
+        try {
+            FunctionVO function = new FunctionVO();
+            function.setUrl("/index/welcome");
+            function.setAppCode(toucan.getAppCode());
+            function.setAdminId(AuthHeaderUtil.getAdminId(request.getHeader(toucan.getAdminAuth().getHttpToucanAuthHeader())));
+            RequestJsonVO requestJsonVO = RequestJsonVOGenerator.generator(toucan.getAppCode(),function);
+            ResultObjectVO resultObjectVO = feignFunctionService.queryOneChildsByAdminIdAndAppCodeAndParentUrl(SignUtil.sign(requestJsonVO),requestJsonVO);
+            if(resultObjectVO.isSuccess())
+            {
+                List<Function> functions = JSONArray.parseArray(JSONObject.toJSONString(resultObjectVO.getData()),Function.class);
+                if(!CollectionUtils.isEmpty(functions))
+                {
+                    List<String> quickButtons = new ArrayList<String>();
+
+                    for(Function buttonFunction:functions)
+                    {
+                        if(buttonFunction.getType().shortValue()==2)
+                        {
+                            quickButtons.add(buttonFunction.getFunctionText());
+                        }
+                    }
+
+                    request.setAttribute("quickButtons",quickButtons);
+                }
+            }
+        }catch(Exception e)
+        {
+            logger.warn(e.getMessage(),e);
+
+            request.setAttribute("quickButtons",new ArrayList<String>());
+        }
+
         return "welcome.html";
     }
 
