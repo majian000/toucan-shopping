18e7a65c6fe4d82e9eda33a4b2ccb16faa6180fe
majian <mmdrss@163.com>
2021-06-12 01:35:31 +0800

    类别列表 创建时间格式化

--- toucan-shopping-modules/category/toucan-shopping-category-entity-vo/src/main/java/com/toucan/shopping/modules/category/entity/Category.java
+++ toucan-shopping-modules/category/toucan-shopping-category-entity-vo/src/main/java/com/toucan/shopping/modules/category/entity/Category.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson.serializer.ToStringSerializer;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -29,7 +30,15 @@ public class Category {
     private Long categorySort; //排序
     private Integer showStatus; //显示状态 0隐藏 1显示
     private String href; //点击类别跳转路径
+
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createDate; //创建时间
+
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateDate; //创建时间
     private String createAdminId; //创建人ID
     private String updateAdminId; //修改人ID
