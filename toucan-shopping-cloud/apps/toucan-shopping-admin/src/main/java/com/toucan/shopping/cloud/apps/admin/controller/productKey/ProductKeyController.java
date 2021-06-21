e03d76a525ed53fee0ee18aa22b358008e6093e9
majian <mmdrss@163.com>
2021-06-21 15:35:23 +0800

    商品属性键管理

---
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/productKey/ProductKeyController.java
@@ -0,0 +1,138 @@
+package com.toucan.shopping.cloud.apps.admin.controller.productKey;
+
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.toucan.shopping.cloud.admin.auth.api.feign.service.FeignFunctionService;
+import com.toucan.shopping.cloud.apps.admin.auth.web.controller.base.UIController;
+import com.toucan.shopping.cloud.area.api.feign.service.FeignAreaService;
+import com.toucan.shopping.cloud.area.api.feign.service.FeignBannerAreaService;
+import com.toucan.shopping.cloud.area.api.feign.service.FeignBannerService;
+import com.toucan.shopping.modules.area.entity.Area;
+import com.toucan.shopping.modules.area.entity.Banner;
+import com.toucan.shopping.modules.area.entity.BannerArea;
+import com.toucan.shopping.modules.area.page.BannerPageInfo;
+import com.toucan.shopping.modules.area.vo.AreaTreeVO;
+import com.toucan.shopping.modules.area.vo.AreaVO;
+import com.toucan.shopping.modules.area.vo.BannerAreaVO;
+import com.toucan.shopping.modules.area.vo.BannerVO;
+import com.toucan.shopping.modules.auth.admin.AdminAuth;
+import com.toucan.shopping.modules.common.generator.IdGenerator;
+import com.toucan.shopping.modules.common.generator.RequestJsonVOGenerator;
+import com.toucan.shopping.modules.common.properties.Toucan;
+import com.toucan.shopping.modules.common.util.AuthHeaderUtil;
+import com.toucan.shopping.modules.common.util.DateUtils;
+import com.toucan.shopping.modules.common.util.SignUtil;
+import com.toucan.shopping.modules.common.vo.RequestJsonVO;
+import com.toucan.shopping.modules.common.vo.ResultObjectVO;
+import com.toucan.shopping.modules.fastdfs.util.FastDFSClient;
+import com.toucan.shopping.modules.layui.vo.TableVO;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 商品属性键管理
+ */
+@Controller
+@RequestMapping("/product/key")
+public class ProductKeyController extends UIController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Value("${toucan.app-code}")
+    private String appCode;
+
+    @Value("${fastdfs.http.url}")
+    private String fastDfsHttpUrl;
+
+    @Autowired
+    private Toucan toucan;
+
+    @Autowired
+    private FastDFSClient fastDFSClient;
+
+
+    @Autowired
+    private FeignFunctionService feignFunctionService;
+
+    @Autowired
+    private FeignBannerAreaService feignBannerAreaService;
+
+    @Autowired
+    private IdGenerator idGenerator;
+
+    @Autowired
+    private FeignBannerService feignBannerService;
+
+    @Autowired
+    private FeignAreaService feignAreaService;
+
+    @AdminAuth(verifyMethod = AdminAuth.VERIFYMETHOD_ADMIN_AUTH,requestType = AdminAuth.REQUEST_FORM)
+    @RequestMapping(value = "/listPage",method = RequestMethod.GET)
+    public String listPage(HttpServletRequest request)
+    {
+        //初始化工具条按钮、操作按钮
+        super.initButtons(request,toucan,"/productKey/listPage",feignFunctionService);
+        return "pages/productKey/list.html";
+    }
+
+
+
+    /**
+     * 查询列表
+     * @param pageInfo
+     * @return
+     */
+    @AdminAuth(verifyMethod = AdminAuth.VERIFYMETHOD_ADMIN_AUTH,requestType = AdminAuth.REQUEST_FORM)
+    @RequestMapping(value = "/list",method = RequestMethod.POST)
+    @ResponseBody
+    public TableVO list(HttpServletRequest request, BannerPageInfo pageInfo)
+    {
+        TableVO tableVO = new TableVO();
+        try {
+            RequestJsonVO requestJsonVO = RequestJsonVOGenerator.generator(toucan.getAppCode(),pageInfo);
+            ResultObjectVO resultObjectVO = feignBannerService.queryListPage(SignUtil.sign(requestJsonVO),requestJsonVO);
+            if(resultObjectVO.getCode() == ResultObjectVO.SUCCESS)
+            {
+                if(resultObjectVO.getData()!=null)
+                {
+                    Map<String,Object> resultObjectDataMap = (Map<String,Object>)resultObjectVO.getData();
+                    tableVO.setCount(Long.parseLong(String.valueOf(resultObjectDataMap.get("total")!=null?resultObjectDataMap.get("total"):"0")));
+                    List<BannerVO> list = JSONArray.parseArray(JSONObject.toJSONString(resultObjectDataMap.get("list")),BannerVO.class);
+                    for(BannerVO bannerVO:list)
+                    {
+                        if(bannerVO.getImgPath()!=null) {
+                            bannerVO.setHttpImgPath(fastDfsHttpUrl + bannerVO.getImgPath());
+                        }
+                    }
+                    if(tableVO.getCount()>0) {
+                        tableVO.setData((List)list);
+                    }
+                }
+            }
+        }catch(Exception e)
+        {
+            tableVO.setMsg("请求失败,请重试");
+            tableVO.setCode(TableVO.FAILD);
+            logger.warn(e.getMessage(),e);
+        }
+        return tableVO;
+    }
+
+
+}
+
