eb186c4b56248209f0f2c0dd9aa662ced1369f87
majian <mmdrss@163.com>
2021-07-12 10:51:01 +0800

    消息服务

---
+++ toucan-shopping-modules/message/toucan-shopping-message-entity-vo/src/main/java/com/toucan/shopping/modules/message/vo/InternalMessageVO.java
@@ -0,0 +1,15 @@
+package com.toucan.shopping.modules.message.vo;
+
+import com.toucan.shopping.modules.message.entity.InternalMessage;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 站内信
+ *
+ * @author majian
+ */
+@Data
+public class InternalMessageVO extends InternalMessage {
+}
