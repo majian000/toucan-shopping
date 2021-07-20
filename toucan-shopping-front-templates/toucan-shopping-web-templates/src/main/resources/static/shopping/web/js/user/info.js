570fc9af03f5b8546057ee8273143558f01ea4fc
majian <mmdrss@163.com>
2021-07-20 11:18:21 +0800

    完善代码

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/user/info.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/user/info.js
@@ -1,16 +1,11 @@
 
 function loadUserInfo()
 {
-
     $.ajax({
         url: basePath+"/api/user/info",
-        type: 'put',
+        type: 'post',
         dataType: 'json',
-        data: JSON.stringify({"uid":window.localStorage.getItem("uid")}),
         cache: false,
-        headers: {
-            "bbs-auth-header":"uid="+window.localStorage.getItem("uid")+";lt="+window.localStorage.getItem("lt"),
-        },
         success: function(res){
 
         }
