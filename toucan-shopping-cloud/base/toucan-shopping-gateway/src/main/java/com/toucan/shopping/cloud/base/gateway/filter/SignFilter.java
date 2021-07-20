a36b08ddf6ebfbc3b21697ed7dc6666a88d6be00
majian <mmdrss@163.com>
2021-07-20 23:40:02 +0800

    网关签名 阻塞读取request body

--- toucan-shopping-cloud/base/toucan-shopping-gateway/src/main/java/com/toucan/shopping/cloud/base/gateway/filter/SignFilter.java
+++ toucan-shopping-cloud/base/toucan-shopping-gateway/src/main/java/com/toucan/shopping/cloud/base/gateway/filter/SignFilter.java
@@ -97,6 +97,16 @@ public class SignFilter implements GlobalFilter, Ordered {
                     CountDownLatch downLatch = new CountDownLatch(1);
                     StringBuilder builder = new StringBuilder();
                     //阻塞读取
+//                    body.doOnComplete(()->{
+//                        downLatch.countDown();
+//                    });
+//                    body.doOnCancel(()->{
+//                        downLatch.countDown();
+//                    });
+//                    body.doOnTerminate(()->{
+//                        downLatch.countDown();
+//                    });
+
                     body.subscribe(dataBuffer -> {
                         try {
                             logger.info(" 开始读取请求体 ");
