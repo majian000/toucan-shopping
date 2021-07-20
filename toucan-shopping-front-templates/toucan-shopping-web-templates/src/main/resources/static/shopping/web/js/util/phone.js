792f3030bed90f10db48fbac6fb391861329cb80
majian <mmdrss@163.com>
2021-07-20 21:29:33 +0800

    个人中心

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/util/phone.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/util/phone.js
@@ -1,9 +1,6 @@
 
 function validPhone(phone)
 {
-    var myreg=myreg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/;
-    if(!myreg.test(phone)) {
-        return false;
-    }
-    return true;
+    var myreg=/^1[34578]\d{9}$/;
+    return myreg.test(phone);
 }
