95472fdfda3967c67b2f43d4624a35773b166388
majian <mmdrss@163.com>
2021-08-02 17:46:33 +0800

    管理端静态页面生成器

--- toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/htmlPage/index/IndexGeneratorController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/htmlPage/index/IndexGeneratorController.java
@@ -2,14 +2,20 @@ package com.toucan.shopping.cloud.apps.admin.controller.htmlPage.index;
 
 
 import com.toucan.shopping.cloud.apps.admin.auth.web.controller.base.UIController;
+import com.toucan.shopping.cloud.apps.admin.vo.htmlPage.HtmlGeneratorTab;
 import com.toucan.shopping.modules.auth.admin.AdminAuth;
+import com.toucan.shopping.modules.common.properties.Toucan;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/index/html")
@@ -18,13 +24,42 @@ public class IndexGeneratorController extends UIController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private Toucan toucan;
 
 
     @AdminAuth(verifyMethod = AdminAuth.VERIFYMETHOD_ADMIN_AUTH,requestType = AdminAuth.REQUEST_FORM)
     @RequestMapping(value = "/indexGeneratorPage",method = RequestMethod.GET)
     public String indexGeneratorPage(HttpServletRequest request)
     {
+        List<HtmlGeneratorTab> releaseHtmlGeneratorTabList = new ArrayList<HtmlGeneratorTab>();
+        List<HtmlGeneratorTab> previewHtmlGeneratorTabList = new ArrayList<HtmlGeneratorTab>();
+        if(toucan.getShoppingPC()!=null&& StringUtils.isNotEmpty(toucan.getShoppingPC().getIpList()))
+        {
+            String ipList = toucan.getShoppingPC().getIpList();
+            if(ipList.indexOf(",")!=-1)
+            {
+                String[] ips = ipList.split(",");
+                if(ips!=null&&ips.length>0)
+                {
+                    for(String ip:ips)
+                    {
+                        HtmlGeneratorTab releaseHtmlGeneratorTable = new HtmlGeneratorTab();
+                        releaseHtmlGeneratorTable.setName(ip);
+                        releaseHtmlGeneratorTable.setContent("<iframe src='http://"+ip+"/htmls/release/index.html' style='width:100%;height:100%' ></iframe>");
+                        releaseHtmlGeneratorTabList.add(releaseHtmlGeneratorTable);
+
+                        HtmlGeneratorTab previewHtmlGeneratorTable = new HtmlGeneratorTab();
+                        previewHtmlGeneratorTable.setName(ip);
+                        previewHtmlGeneratorTable.setContent("<iframe src='http://"+ip+"/htmls/preview/index.html' style='width:100%;height:100%' ></iframe>");
+                        previewHtmlGeneratorTabList.add(previewHtmlGeneratorTable);
+                    }
+                }
+            }
+        }
         return "pages/htmlPage/index/index.html";
     }
+
+
 
 }
---
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/vo/htmlPage/HtmlGeneratorTab.java
@@ -0,0 +1,24 @@
+package com.toucan.shopping.cloud.apps.admin.vo.htmlPage;
+
+
+import lombok.Data;
+
+/**
+ * 首页选项卡
+ */
+@Data
+public class HtmlGeneratorTab {
+
+    /**
+     * 选项卡名称
+     */
+    private String name;
+
+    /**
+     * 选项卡内容
+     */
+    private String content;
+
+
+
+}
