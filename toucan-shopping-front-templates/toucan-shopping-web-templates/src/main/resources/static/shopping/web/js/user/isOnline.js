e99a74a0ac166b0b04b3c9a33bb2efc408448612
majian <mmdrss@163.com>
2021-08-13 20:27:46 +0800

    完善样式

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/user/isOnline.js
+++
@@ -1,9 +0,0 @@
-// $(function () {
-//
-//     $.post(basePath+"/api/user/is/online",null,function(result){
-//         if(result.code<=0||result.data==false)
-//         {
-//             window.location.href=basePath+"/page/user/login";
-//         }
-//     });
-// });
