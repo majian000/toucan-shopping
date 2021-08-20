3b20802652ce12d77887cca68b13627e114ab054
majian <mmdrss@163.com>
2021-08-20 14:52:37 +0800

    卖家中心

---
+++ toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/controller/seller/SellerManagerPageController.java
@@ -0,0 +1,53 @@
+package com.toucan.shopping.cloud.apps.seller.web.controller.seller;
+
+import com.toucan.shopping.cloud.apps.seller.web.controller.BaseController;
+import com.toucan.shopping.cloud.seller.api.feign.service.FeignSellerShopService;
+import com.toucan.shopping.cloud.user.api.feign.service.FeignUserService;
+import com.toucan.shopping.modules.auth.user.UserAuth;
+import com.toucan.shopping.modules.common.generator.RequestJsonVOGenerator;
+import com.toucan.shopping.modules.common.properties.Toucan;
+import com.toucan.shopping.modules.common.util.UserAuthHeaderUtil;
+import com.toucan.shopping.modules.common.vo.RequestJsonVO;
+import com.toucan.shopping.modules.common.vo.ResultObjectVO;
+import com.toucan.shopping.modules.user.vo.UserVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * 卖家中心
+ */
+@Controller("sellerManagerPageController")
+@RequestMapping("/page/seller/manager")
+public class SellerManagerPageController extends BaseController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private FeignUserService feignUserService;
+
+    @Autowired
+    private FeignSellerShopService feignSellerShopService;
+
+    @Autowired
+    private Toucan toucan;
+
+
+    /**
+     * 管理中心
+     * @return
+     */
+    @UserAuth(requestType = UserAuth.REQUEST_FORM)
+    @RequestMapping("/page")
+    public String info(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+    {
+        return "seller/manager";
+    }
+
+}
