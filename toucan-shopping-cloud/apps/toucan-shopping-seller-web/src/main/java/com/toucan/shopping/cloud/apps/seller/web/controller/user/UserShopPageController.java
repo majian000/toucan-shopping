ee4a29a3b501306415d5638bd582601ba46840a9
majian <mmdrss@163.com>
2021-08-24 21:17:24 +0800

    店铺注册

--- toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/controller/user/UserShopPageController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/controller/user/UserShopPageController.java
@@ -1,6 +1,7 @@
 package com.toucan.shopping.cloud.apps.seller.web.controller.user;
 
 import com.toucan.shopping.cloud.apps.seller.web.controller.BaseController;
+import com.toucan.shopping.cloud.seller.api.feign.service.FeignSellerShopService;
 import com.toucan.shopping.cloud.user.api.feign.service.FeignUserService;
 import com.toucan.shopping.modules.auth.user.UserAuth;
 import com.toucan.shopping.modules.common.generator.RequestJsonVOGenerator;
@@ -8,6 +9,7 @@ import com.toucan.shopping.modules.common.properties.Toucan;
 import com.toucan.shopping.modules.common.util.UserAuthHeaderUtil;
 import com.toucan.shopping.modules.common.vo.RequestJsonVO;
 import com.toucan.shopping.modules.common.vo.ResultObjectVO;
+import com.toucan.shopping.modules.seller.entity.SellerShop;
 import com.toucan.shopping.modules.user.vo.UserVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +34,8 @@ public class UserShopPageController extends BaseController {
     @Autowired
     private FeignUserService feignUserService;
 
+    @Autowired
+    private FeignSellerShopService feignSellerShopService;
 
     @Autowired
     private Toucan toucan;
@@ -57,6 +61,26 @@ public class UserShopPageController extends BaseController {
                 boolean result = Boolean.valueOf(String.valueOf(resultObjectVO.getData()));
                 if(result)
                 {
+                    SellerShop querySellerShop = new SellerShop();
+                    querySellerShop.setUserMainId(userVO.getUserMainId());
+                    requestJsonVO = RequestJsonVOGenerator.generator(this.getAppCode(), querySellerShop);
+                    //判断是个人店铺还是企业店铺
+                    resultObjectVO = feignSellerShopService.findByUser(requestJsonVO.sign(),requestJsonVO);
+                    if(resultObjectVO.isSuccess())
+                    {
+                        //该账号存在店铺
+                        SellerShop sellerShop = resultObjectVO.formatData(SellerShop.class);
+                        if(sellerShop!=null)
+                        {
+                            //个人店铺
+                            if(sellerShop.getType().intValue()==1)
+                            {
+                                httpServletRequest.setAttribute("sellerShop",sellerShop);
+                                return "shop/userShop/info";
+                            }
+                        }
+
+                    }
                     return "shop/userShop/regist";
                 }else{
                     //重定向到实名审核页面
