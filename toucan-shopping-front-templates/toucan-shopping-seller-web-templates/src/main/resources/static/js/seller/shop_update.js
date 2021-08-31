08afafd9dadf19837ca25d7404ae1b7a13daf684
majian <mmdrss@163.com>
2021-08-31 15:58:11 +0800

    修改店铺信息

---
+++ toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/shop_update.js
@@ -0,0 +1,47 @@
+
+
+
+function updateUserShop()
+{
+    var nameValue=$("#name").val();
+    var vcodeValue=$("#utm_vcode").val();
+
+    $("#name_msg").text(" ");
+    $("#utm_vcode_msg").text(" ");
+
+    if(nameValue=="")
+    {
+        $("#name_msg").text("请输入店铺名称");
+        return ;
+    }
+    if(vcodeValue=="")
+    {
+        $("#utm_vcode_msg").text("请输入验证码");
+        return ;
+    }
+    $.ajax({
+        type: "POST",
+        url: basePath+'/api/user/shop/regist',
+        contentType: "application/json;charset=utf-8",
+        data:  JSON.stringify({"name":nameValue,"vcode":vcodeValue}),
+        dataType: "json",
+        success: function (data) {
+            if(data.code==401)
+            {
+                window.location.href=basePath+data.data;
+            }else  if(data.code==0)
+            {
+                $("#refreshCaptcha").attr("src",basePath+"/api/user/vcode?"+new Date().getTime());
+                $("#tn_msg").text(data.msg);
+            }else if(data.code==1)
+            {
+                window.location.href=basePath+"/page/shop/submit_success";
+            }
+        },
+        error: function (result) {
+            $("#refreshCaptcha").attr("src",basePath+"/api/user/vcode?"+new Date().getTime());
+            $("#tn_msg").text("请求失败,请重试");
+        }
+    });
+
+}
