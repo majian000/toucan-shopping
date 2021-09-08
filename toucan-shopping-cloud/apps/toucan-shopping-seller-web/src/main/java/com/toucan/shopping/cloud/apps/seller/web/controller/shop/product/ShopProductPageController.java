8ddd2886622488439f5c34806722d9581ccea460
majian <mmdrss@163.com>
2021-09-08 11:34:51 +0800

    发布商品选择分类

---
+++ toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/controller/shop/product/ShopProductPageController.java
@@ -0,0 +1,45 @@
+package com.toucan.shopping.cloud.apps.seller.web.controller.shop.product;
+
+import com.toucan.shopping.cloud.apps.seller.web.controller.BaseController;
+import com.toucan.shopping.cloud.seller.api.feign.service.FeignSellerShopService;
+import com.toucan.shopping.cloud.user.api.feign.service.FeignUserService;
+import com.toucan.shopping.modules.auth.user.UserAuth;
+import com.toucan.shopping.modules.common.generator.RequestJsonVOGenerator;
+import com.toucan.shopping.modules.common.properties.Toucan;
+import com.toucan.shopping.modules.common.util.UserAuthHeaderUtil;
+import com.toucan.shopping.modules.common.vo.RequestJsonVO;
+import com.toucan.shopping.modules.common.vo.ResultObjectVO;
+import com.toucan.shopping.modules.image.upload.service.ImageUploadService;
+import com.toucan.shopping.modules.seller.entity.SellerShop;
+import com.toucan.shopping.modules.seller.vo.SellerShopVO;
+import com.toucan.shopping.modules.user.vo.UserVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * 店铺商品信息
+ */
+@Controller("shopProductPageController")
+@RequestMapping("/page/shop/product")
+public class ShopProductPageController extends BaseController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Toucan toucan;
+
+
+    @UserAuth(requestType = UserAuth.REQUEST_FORM)
+    @RequestMapping("/release")
+    public String submit_success(HttpServletRequest request){
+        return "product/release_product";
+    }
+
+}
