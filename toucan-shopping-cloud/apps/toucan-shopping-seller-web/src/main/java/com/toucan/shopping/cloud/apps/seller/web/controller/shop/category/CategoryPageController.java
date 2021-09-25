49f2f47785403b9936e99b9f66bf5850b48d2d12
majian <mmdrss@163.com>
2021-09-25 17:14:26 +0800

    店铺分类

---
+++ toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/controller/shop/category/CategoryPageController.java
@@ -0,0 +1,32 @@
+package com.toucan.shopping.cloud.apps.seller.web.controller.shop.category;
+
+import com.toucan.shopping.cloud.apps.seller.web.controller.BaseController;
+import com.toucan.shopping.modules.auth.user.UserAuth;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 店铺分类
+ */
+@Controller("categoryPageController")
+@RequestMapping("/page/shop/category")
+public class CategoryPageController  extends BaseController {
+
+
+
+    @UserAuth(requestType = UserAuth.REQUEST_FORM)
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request)
+    {
+        return "shop/category/index";
+    }
+
+    @UserAuth(requestType = UserAuth.REQUEST_FORM)
+    @RequestMapping("/list")
+    public String list(HttpServletRequest request)
+    {
+        return "shop/category/list";
+    }
+}
