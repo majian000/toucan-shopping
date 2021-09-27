3b11820df632165c20171431143e4cff25728704
majian <mmdrss@163.com>
2021-09-27 17:11:08 +0800

    添加分类

---
+++ toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/shop/category/add.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('#addsc').on('click', function(){
+        layer.open({
+            type: 2,
+            title: '添加类别',
+            fix: false,
+            shadeClose: true,
+            maxmin: true,
+            area: ['700px', '670px'],
+            content: 'chongzhits.html'
+        });
+
+    });
+});
