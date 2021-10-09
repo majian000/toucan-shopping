f038ee7dae7a2337c783930bad66f6a24126fcc5
majian <mmdrss@163.com>
2021-10-09 14:28:00 +0800

    发布商品 向导页

--- toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/selectShopCategory/selectShopCategory.js
+++ toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/selectShopCategory/selectShopCategory.js
@@ -7,38 +7,49 @@
 
 
 
-var expressP, expressC, expressD, expressArea, areaCont;
+var expressShopCategoryP, expressShopCategoryC,  areaShopCategoryCont;
 var arrow = " <font>&gt;</font> ";
 
 /*初始化一级目录*/
 function intRootShopCategory() {
-	areaCont = "";
+	areaShopCategoryCont = "";
 	for (var i=0; i<shop_category_list.length; i++) {
-		areaCont += '<li onClick="selectShopCategoryLevel1(' + i + ');"><a href="javascript:void(0)">' + shop_category_list[i].name + '</a></li>';
+		areaShopCategoryCont += '<li onClick="selectShopCategoryLevel1(' + i + ');"><a href="javascript:void(0)">' + shop_category_list[i].name + '</a></li>';
 	}
-	$("#shop_category_sort1").html(areaCont);
+	$("#shop_category_sort1").html(areaShopCategoryCont);
 }
 intRootShopCategory();
 
 /*选择一级目录*/
 function selectShopCategoryLevel1(p) {
-	areaCont = "";
+	areaShopCategoryCont = "";
 	for (var j=0; j<shop_category_list[p].children.length; j++) {
-		areaCont += '<li onClick="selectShopCategoryLevel2(' + p + ',' + j + ');"><a href="javascript:void(0)">' + shop_category_list[p].children[j].name + '</a></li>';
+		areaShopCategoryCont += '<li onClick="selectShopCategoryLevel2(' + p + ',' + j + ');"><a href="javascript:void(0)">' + shop_category_list[p].children[j].name + '</a></li>';
 	}
-	$("#shop_category_sort2").html(areaCont).show();
-	$("#shop_category_sort1 li").eq(p).addClass("active").siblings("li").removeClass("active");
-	expressP = shop_category_list[p].name;
-	$("#selectedShopCategorySort").html(expressP);
-	$("#releaseBtn").removeAttr("disabled");
+	$("#shop_category_sort2").html(areaShopCategoryCont).show();
+	if($("#shop_category_sort1 li").eq(p).hasClass("active"))
+	{
+		$("#shop_category_sort1 li").eq(p).removeClass("active");
+		expressShopCategoryP="";
+	}else{
+		$("#shop_category_sort1 li").eq(p).addClass("active").siblings("li").removeClass("active");
+		expressShopCategoryP = shop_category_list[p].name;
+	}
+	$("#selectedShopCategorySort").html(expressShopCategoryP);
 }
 
 /*选择二级目录*/
 function selectShopCategoryLevel2(p,c) {
-	expressC = "";
-	$("#shop_category_sort2 li").eq(c).addClass("active").siblings("li").removeClass("active");
-	expressC = expressP + arrow + shop_category_list[p].children[c].name;
-	$("#selectedShopCategorySort").html(expressC);
+	expressShopCategoryC = "";
+	if($("#shop_category_sort2 li").eq(c).hasClass("active"))
+	{
+		$("#shop_category_sort2 li").eq(c).removeClass("active");
+		expressShopCategoryC=expressShopCategoryP;
+	}else{
+		$("#shop_category_sort2 li").eq(c).addClass("active").siblings("li").removeClass("active");
+		expressShopCategoryC = expressShopCategoryP + arrow + shop_category_list[p].children[c].name;
+	}
+	$("#selectedShopCategorySort").html(expressShopCategoryC);
 }
 
 
--- toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/product/release_product.js
+++ toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/product/release_product.js
@@ -47,6 +47,10 @@ $.extend(validateFunction, {
     },
     step4_form_validate:function() {
         return true;
+    },
+    step5_form_validate:function() {
+        $("#vcode").jdValidate(validatePrompt.vcode, validateFunction.vcode, true);
+        return validateFunction.FORM_submit(["#vcode"]);
     }
 });
 
@@ -63,3 +67,38 @@ setTimeout(function() {
 }, 0);
 //验证码验证
 $("#vcode").jdValidate(validatePrompt.vcode, validateFunction.vcode);
+
+
+
+
+
+$("#ppfbtn").click(function() {
+    var flag = validateFunction.step5_form_validate();
+    if (flag) {
+        var fields = $('#productReleaseForm').serializeArray();
+        var params = {}; //声明一个对象
+        $.each(fields, function(index, field) {
+            params[field.name] = field.value; //通过变量，将属性值，属性一起放到对象中
+        });
+        // $.ajax({
+        //     type: "POST",
+        //     url: basePath+"/api/user/edit/info",
+        //     contentType: "application/json;charset=utf-8",
+        //     data: JSON.stringify(params),
+        //     dataType: "json",
+        //     success: function (result) {
+        //         if(result.code<=0)
+        //         {
+        //             $("#vcode").val("");
+        //             $("#edit_info_msg").text(result.msg);
+        //             $("#refreshCaptcha").attr("src",basePath+"/api/user/vcode?"+new Date().getTime());
+        //         }else{
+        //             window.location.href=basePath+"/page/user/info";
+        //         }
+        //     },
+        //     error: function (result) {
+        //         $("#edit_info_msg").text("修改失败,请稍后重试");
+        //     }
+        // });
+    }
+});
