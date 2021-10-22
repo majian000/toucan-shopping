4e799a36daf0ce46c0cbeb021a2cc7b5e0606782
majian <mmdrss@163.com>
2021-10-22 17:21:03 +0800

    全局日志处理

---
+++ toucan-shopping-modules/common/toucan-shopping-log/src/main/java/com/toucan/shopping/modules/log/ToucanLog.java
@@ -0,0 +1,28 @@
+package com.toucan.shopping.modules.log;
+
+/**
+ * 犀鸟日志处理
+ * @author majian
+ */
+public class ToucanLog {
+
+
+    /**
+     * warn级别日志
+     * @param msg
+     * @param ex
+     */
+    static void warn(String msg, Throwable ex){
+
+    }
+
+    /**
+     * info级别日志
+     * @param msg
+     */
+    static void info(String msg){
+
+    }
+
+
+}
