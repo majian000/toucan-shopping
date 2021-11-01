87bbc165542477cd80d3e5fc40491684c40d3a21
majian <mmdrss@163.com>
2021-11-01 20:29:51 +0800

    修改bug

--- toucan-shopping-cloud/services/toucan-shopping-admin-auth/src/main/java/com/toucan/shopping/cloud/admin/auth/app/CloudAdminAuthServiceApplication.java
+++ toucan-shopping-cloud/services/toucan-shopping-admin-auth/src/main/java/com/toucan/shopping/cloud/admin/auth/app/CloudAdminAuthServiceApplication.java
@@ -1,12 +1,13 @@
 package com.toucan.shopping.cloud.admin.auth.app;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @EnableDiscoveryClient
 @MapperScan({"com.toucan.shopping.modules.admin.auth.mapper",
         "com.toucan.shopping.modules.common.persistence.mapper"})
