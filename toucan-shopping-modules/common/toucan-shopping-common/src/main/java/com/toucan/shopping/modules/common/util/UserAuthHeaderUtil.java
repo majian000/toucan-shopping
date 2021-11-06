c80034d99a0bb02f7869de6fa9c9b8d6ea830b15
majian <mmdrss@163.com>
2021-11-06 15:53:40 +0800

    完善获取请求头中用户ID

--- toucan-shopping-modules/common/toucan-shopping-common/src/main/java/com/toucan/shopping/modules/common/util/UserAuthHeaderUtil.java
+++ toucan-shopping-modules/common/toucan-shopping-common/src/main/java/com/toucan/shopping/modules/common/util/UserAuthHeaderUtil.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 public class UserAuthHeaderUtil {
 
 
+
     public static String getUserMainId(String authHeader) throws Exception {
         if(StringUtils.isEmpty(authHeader))
         {
@@ -16,6 +17,10 @@ public class UserAuthHeaderUtil {
         if(userId.indexOf(";")!=-1)
         {
             userId=userId.substring(0,userId.indexOf(";"));
+        }
+        if(StringUtils.isEmpty(userId))
+        {
+            throw new IllegalArgumentException("从请求头中没有找到用户ID 请求头值为:"+authHeader);
         }
         return userId;
     }
@@ -33,6 +38,10 @@ public class UserAuthHeaderUtil {
         if(token.indexOf(";")!=-1)
         {
             token=token.substring(0,token.indexOf(";"));
+        }
+        if(StringUtils.isEmpty(token))
+        {
+            throw new IllegalArgumentException("从请求头中没有找到用户token 请求头值为:"+authHeader);
         }
         return token;
     }
