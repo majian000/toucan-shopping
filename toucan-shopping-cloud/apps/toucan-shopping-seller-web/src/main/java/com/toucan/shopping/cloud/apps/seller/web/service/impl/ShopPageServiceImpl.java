f94c16d3b10cba3f159381e778b5bd87dbd3a594
majian <mmdrss@163.com>
2021-11-21 21:13:05 +0800

    店铺注册

--- toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/service/impl/ShopPageServiceImpl.java
+++ toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/service/impl/ShopPageServiceImpl.java
@@ -66,7 +66,7 @@ public class ShopPageServiceImpl implements ShopPageService {
                         SellerShopVO sellerShopVO = resultObjectVO.formatData(SellerShopVO.class);
                         if(sellerShopVO==null)
                         {
-                            return "shop/select_regist_type";
+                            return "/htmls/release/freeShop";
                         }
 
                         //设置店铺logo
