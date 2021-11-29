443b6dca0306dea4d920e8de94671cf8e47c7d3a
majian <mmdrss@163.com>
2021-11-29 14:06:16 +0800

    分类 导航条

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/category/categeory_navigation.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/category/categeory_navigation.js
@@ -9,7 +9,7 @@ $(function(){
             if(result.code>0)
             {
                 var htmls = "";
-                var categorys = result.data.categorys;
+                var categorys = result.data;
                 if(categorys!=null&&categorys.length>0)
                 {
                     for(var i=0;i<categorys.length;i++)
@@ -19,10 +19,10 @@ $(function(){
                         {
                             htmls+="<li>";
                             htmls+="<div class='fj'>";
-                            htmls+="<span class='n_img'><span></span><img src='"+category+"' /></span>";
-                            htmls+="<span class='fl'>"+category.rootLinks+"</span>";
+                            htmls+="<span class='n_img'><span></span><img src='"+category.icon+"' /></span>";
+                            htmls+="<span class='fl'>"+(category.rootLinks!=null?category.rootLinks:"")+"</span>";
                             htmls+="</div>";
-                            htmls+="<div class='zj' style='"+category.pcIndexStyle+"' >";
+                            htmls+="<div class='zj' style='"+(category.pcIndexStyle!=null?category.pcIndexStyle:"")+"' >";
                             htmls+="<div class='zj_l'>";
                             var categoryChildren = category.children;
                             if(categoryChildren!=null&&categoryChildren.length>0)
@@ -32,15 +32,7 @@ $(function(){
                                     var cc = categoryChildren[j];
                                     if(cc.showStatus!=null && cc.showStatus==1) {
                                         htmls +=" <div class='zj_l_c'>";
-                                        if(cc.href==null)
-                                        {
-                                            cc.href = "#";
-                                        }
-                                        if(cc.name==null)
-                                        {
-                                            cc.name="";
-                                        }
-                                        htmls += "<h2><a href='"+cc.href+"'>"+cc.name+"</a></h2>";
+                                        htmls += "<h2><a href='"+(cc.href!=null?cc.href:"#")+"'>"+(cc.name!=null?cc.name:"")+"</a></h2>";
                                         htmls+="<div style='padding-left: 10px;'>";
                                         var categoryChildChildren = cc.children;
                                         if(categoryChildChildren!=null&&categoryChildChildren.length>0) {
@@ -48,20 +40,12 @@ $(function(){
                                                 var ccc = categoryChildChildren[p];
                                                 if(ccc.showStatus!=null && ccc.showStatus==1)
                                                 {
-                                                    if(ccc.href==null)
-                                                    {
-                                                        ccc.href = "#";
-                                                    }
-                                                    if(ccc.name==null)
-                                                    {
-                                                        ccc.name="";
-                                                    }
-                                                    htmls+="<a href='"+ccc.href+"' class='ac' >"+ccc.name+"</a>|";
+                                                    htmls+="<a href='"+(ccc.href!=null?ccc.href:"#")+"' class='ac' >"+(ccc.name!=null?ccc.name:"")+"</a>|";
                                                 }
                                             }
-                                            htmls+="</div>";
-                                            htmls+="</div>";
                                         }
+                                        htmls+="</div>";
+                                        htmls+="</div>";
                                     }
                                 }
                             }
@@ -73,11 +57,10 @@ $(function(){
                             htmls+="</div>";
                             htmls+="</li>";
                         }
-
-
-
                     }
                 }
+                window.console.log(htmls);
+                $("#categoryListUl").html(htmls);
             }
         }
     });
