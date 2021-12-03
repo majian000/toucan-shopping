9f4c65089b6bfc78694609ceb9608c43f118c14d
majian <mmdrss@163.com>
2021-12-03 18:28:25 +0800

    个人中心 修改资料

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/user/editInfo.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/user/editInfo.js
@@ -1,125 +1,34 @@
-$.extend(validatePrompt, {
-    nickname:{
-        onFocus:"2-15位字符，可由中文或英文组成",
-        succeed:"",
-        isNull:"请输入昵称",
-        error:{
-            badLength:"昵称长度只能在2-15位字符之间",
-            badFormat:"昵称只能由中文或英文组成"
-        }
-    },
-    personalizedSignature:{
-        onFocus:"2-50位字符，可由中文或英文组成",
-        succeed:"",
-        isNull:"请输入个性签名",
-        error:{
-            badLength:"个性签名长度只能在2-50位字符之间",
-            badFormat:"个性签名只能由中文或英文组成"
-        }
-    },
-    vcode:{
-        isNull:"请输入验证码"
-    }
-});
-
-
-
-$.extend(validateFunction, {
-    nickname:function(option) {
-        var nickname = option.value.replace(" ","");
-        var length = validateRules.betweenLength(nickname, 2, 15);
-        var format = validateRules.isNickname(nickname);
-        if (!length) {
-            validateSettings.error.run(option, option.prompts.error.badLength);
-            return;
-        } else {
-            if (!format) {
-                validateSettings.error.run(option, option.prompts.error.badFormat);
-                return;
-            }
-        }
-        validateSettings.succeed.run(option);
-    },
-    personalizedSignature:function(option) {
-        var personalizedSignature = option.value.replace(" ","");
-        var length = validateRules.betweenLength(personalizedSignature, 2, 50);
-        var format = validateRules.isPersonalizedSignature(personalizedSignature);
-        if (!length) {
-            validateSettings.error.run(option, option.prompts.error.badLength);
-            return;
-        } else {
-            if (!format) {
-                validateSettings.error.run(option, option.prompts.error.badFormat);
-                return;
-            }
-        }
-        validateSettings.succeed.run(option);
-    },
-    vcode:function(option) {
-        var format = validateRules.isNull(option.value);
-        if (format) {
-            validateSettings.error.run(option, option.prompts.isNull);
-            return;
-        }
-        validateSettings.succeed.run(option);
-    },
-    form_validate:function() {
-        $("#nickname").jdValidate(validatePrompt.nickname, validateFunction.nickname, true);
-        $("#personalizedSignature").jdValidate(validatePrompt.personalizedSignature, validateFunction.personalizedSignature, true);
-        $("#vcode").jdValidate(validatePrompt.vcode, validateFunction.vcode, true);
-        return validateFunction.FORM_submit(["#nickname","#vcode"]);
-    }
-});
-
-function init_events(){
-
-    //默认离开获得焦点
-    setTimeout(function() {
-        $("#nickname").get(0).focus();
-        $("#personalizedSignature").get(0).focus();
-    }, 0);
-    //用户名验证
-    $("#nickname").jdValidate(validatePrompt.nickname, validateFunction.nickname);
-
-    //个性签名验证
-    $("#personalizedSignature").jdValidate(validatePrompt.personalizedSignature, validateFunction.personalizedSignature);
-
-    setTimeout(function() {
-        $("#vcode").get(0).focus();
-    }, 0);
-    //验证码验证
-    $("#vcode").jdValidate(validatePrompt.vcode, validateFunction.vcode);
-
-}
 
 
 $("#ueibtn").click(function() {
-    var flag = validateFunction.form_validate();
-    if (flag) {
-        var fields = $('#userEditInfo').serializeArray();
-        var params = {}; //声明一个对象
-        $.each(fields, function(index, field) {
-            params[field.name] = field.value; //通过变量，将属性值，属性一起放到对象中
-        });
-        $.ajax({
-            type: "POST",
-            url: basePath+"/api/user/edit/info",
-            contentType: "application/json;charset=utf-8",
-            data: JSON.stringify(params),
-            dataType: "json",
-            success: function (result) {
-                if(result.code<=0)
-                {
-                    $("#vcode").val("");
-                    $("#edit_info_msg").text(result.msg);
-                    $("#refreshCaptcha").attr("src",basePath+"/api/user/vcode?"+new Date().getTime());
-                }else{
-                    window.location.href=basePath+"/page/user/info";
-                }
-            },
-            error: function (result) {
-                $("#edit_info_msg").text("修改失败,请稍后重试");
-            }
-        });
+
+    if(!checkInputFunction($('#ueibtn'),2)){
+        return false;
     }
+
+    var fields = $('#userEditInfo').serializeArray();
+    var params = {}; //声明一个对象
+    $.each(fields, function(index, field) {
+        params[field.name] = field.value; //通过变量，将属性值，属性一起放到对象中
+    });
+    $.ajax({
+        type: "POST",
+        url: basePath+"/api/user/edit/info",
+        contentType: "application/json;charset=utf-8",
+        data: JSON.stringify(params),
+        dataType: "json",
+        success: function (result) {
+            if(result.code<=0)
+            {
+                $("#vcode").val("");
+                $("#edit_info_msg").text(result.msg);
+                $("#refreshCaptcha").attr("src",basePath+"/api/user/vcode?"+new Date().getTime());
+            }else{
+                window.location.href=basePath+"/page/user/info";
+            }
+        },
+        error: function (result) {
+            $("#edit_info_msg").text("修改失败,请稍后重试");
+        }
+    });
 });
