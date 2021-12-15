3c97d7d787109f8f1582dc117c4fd7047c0fc96d
majian <mmdrss@163.com>
2021-12-15 11:31:45 +0800

    用户头像审核

--- toucan-shopping-cloud/apps/toucan-shopping-web/src/main/java/com/toucan/shopping/cloud/apps/web/controller/user/UserHeadSculptureApproveApiController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-web/src/main/java/com/toucan/shopping/cloud/apps/web/controller/user/UserHeadSculptureApproveApiController.java
@@ -65,7 +65,7 @@ public class UserHeadSculptureApproveApiController extends BaseController {
 
 
 
-    @UserAuth(verifyMethod = UserAuth.VERIFYMETHOD_USER_AUTH,requestType = UserAuth.REQUEST_AJAX)
+    @UserAuth(requestType = UserAuth.REQUEST_AJAX)
     @RequestMapping(value="/save")
     @ResponseBody
     public ResultObjectVO save(HttpServletRequest request,UserHeadSculptureApproveVO userHeadSculptureApproveVO){
