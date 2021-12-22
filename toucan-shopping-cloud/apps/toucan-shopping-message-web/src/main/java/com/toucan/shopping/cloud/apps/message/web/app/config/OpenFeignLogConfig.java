a98d6853aff1b0d6746cb1dd03ba3b9acfb6c49a
majian <mmdrss@163.com>
2021-12-22 17:34:57 +0800

    消息中心 Web端

---
+++ toucan-shopping-cloud/apps/toucan-shopping-message-web/src/main/java/com/toucan/shopping/cloud/apps/message/web/app/config/OpenFeignLogConfig.java
@@ -0,0 +1,18 @@
+package com.toucan.shopping.cloud.apps.message.web.app.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * feign http请求开启打印日志
+ */
+@Configuration
+public class OpenFeignLogConfig {
+
+    @Bean
+    Logger.Level feignLoggerLeave(){
+        return Logger.Level.FULL;
+    }
+
+}
