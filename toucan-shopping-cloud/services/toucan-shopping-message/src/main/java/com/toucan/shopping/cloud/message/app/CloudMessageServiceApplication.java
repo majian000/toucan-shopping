9cc3e8a69637af1fc666879b4d98ad8e2f05db8d
majian <mmdrss@163.com>
2021-12-22 17:55:28 +0800

    消息服务

--- toucan-shopping-cloud/services/toucan-shopping-message/src/main/java/com/toucan/shopping/cloud/message/app/CloudMessageServiceApplication.java
+++ toucan-shopping-cloud/services/toucan-shopping-message/src/main/java/com/toucan/shopping/cloud/message/app/CloudMessageServiceApplication.java
@@ -1,4 +1,4 @@
-package com.toucan.shopping.cloud.user.app;
+package com.toucan.shopping.cloud.message.app;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @EnableDiscoveryClient
 @MapperScan({"com.toucan.shopping.modules.message.mapper",
         "com.toucan.shopping.modules.common.persistence.mapper"})
