7eefb03474e097311e6d0647eb3d6a727506de78
majian <mmdrss@163.com>
2021-12-22 17:02:31 +0800

    消息中心 Web端

---
+++ toucan-shopping-cloud/apps/toucan-shopping-message-web/src/main/java/com/toucan/shopping/cloud/apps/message/web/app/CloudMessageWebApplication.java
@@ -0,0 +1,30 @@
+package com.toucan.shopping.cloud.apps.message.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "com.toucan.shopping")
+@ComponentScan("com.toucan.shopping")
+@EnableHystrix
+public class CloudMessageWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMessageWebApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return  new RestTemplate();
+    }
+
+}
