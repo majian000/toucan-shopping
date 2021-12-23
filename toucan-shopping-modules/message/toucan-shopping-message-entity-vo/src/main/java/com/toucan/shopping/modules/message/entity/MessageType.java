966a48958fc0972524e9b5c8028547eaaa287b00
majian <mmdrss@163.com>
2021-12-23 11:53:36 +0800

    消息服务 service dao

---
+++ toucan-shopping-modules/message/toucan-shopping-message-entity-vo/src/main/java/com/toucan/shopping/modules/message/entity/MessageType.java
@@ -0,0 +1,58 @@
+package com.toucan.shopping.modules.message.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * 消息类型
+ *
+ * @author majian
+ */
+@Data
+public class MessageType {
+
+
+    @JsonFormat(shape=JsonFormat.Shape.STRING)
+    @JSONField(serializeUsing = ToStringSerializer.class)
+    private Long id; //主键
+
+    /**
+     * 消息类型编码
+     */
+    private String code;
+
+    /**
+     * 消息类型名称
+     */
+    private String name;
+
+    /**
+     * 所属应用 (软关联,直接存储应用编码符串)
+     */
+    private String appCode;
+
+    /**
+     * 创建时间
+     */
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createDate;
+
+    /**
+     * 修改时间
+     */
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateDate;
+
+
+    /**
+     * 删除状态 0未删除 1已删除
+     */
+    private Short deleteStatus;
+}
---
+++ toucan-shopping-modules/message/toucan-shopping-message-entity-vo/src/main/java/com/toucan/shopping/modules/message/vo/MessageTypeVO.java
@@ -0,0 +1,13 @@
+package com.toucan.shopping.modules.message.vo;
+
+import com.toucan.shopping.modules.message.entity.InternalMessage;
+import com.toucan.shopping.modules.message.entity.MessageType;
+import lombok.Data;
+
+/**
+ * 消息类型
+ * @author majian
+ */
+@Data
+public class MessageTypeVO extends MessageType {
+}
