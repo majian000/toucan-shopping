1d1bd8b4189f0a16297f99359c4b48310ca064fc
majian <mmdrss@163.com>
2021-12-23 13:59:49 +0800

    消息服务 消息类型

---
+++ toucan-shopping-cloud/services/toucan-shopping-message/src/main/java/com/toucan/shopping/cloud/message/controller/MessageTypeController.java
@@ -0,0 +1,114 @@
+package com.toucan.shopping.cloud.message.controller;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.toucan.shopping.modules.common.generator.IdGenerator;
+import com.toucan.shopping.modules.common.vo.RequestJsonVO;
+import com.toucan.shopping.modules.common.vo.ResultObjectVO;
+import com.toucan.shopping.modules.common.vo.ResultVO;
+import com.toucan.shopping.modules.message.redis.MessageTypeKey;
+import com.toucan.shopping.modules.message.service.MessageTypeService;
+import com.toucan.shopping.modules.message.vo.MessageTypeVO;
+import com.toucan.shopping.modules.skylark.lock.service.SkylarkLock;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 收货地址 增删改查
+ */
+@RestController
+@RequestMapping("/messageType")
+public class MessageTypeController {
+
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private IdGenerator idGenerator;
+
+    @Autowired
+    private SkylarkLock skylarkLock;
+
+    @Autowired
+    private MessageTypeService messageTypeService;
+
+
+    @RequestMapping(value="/save",produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public ResultObjectVO save(@RequestBody RequestJsonVO requestJsonVO){
+        ResultObjectVO resultObjectVO = new ResultObjectVO();
+        if(requestJsonVO==null)
+        {
+            resultObjectVO.setCode(ResultObjectVO.FAILD);
+            resultObjectVO.setMsg("保存失败,没有找到请求对象");
+            return resultObjectVO;
+        }
+        if (StringUtils.isEmpty(requestJsonVO.getAppCode())) {
+            resultObjectVO.setCode(ResultObjectVO.FAILD);
+            resultObjectVO.setMsg("保存失败,没有找到应用编码");
+            return resultObjectVO;
+        }
+        MessageTypeVO messageTypeVO = JSONObject.parseObject(requestJsonVO.getEntityJson(), MessageTypeVO.class);
+        if(StringUtils.isEmpty(messageTypeVO.getName()))
+        {
+            resultObjectVO.setCode(ResultObjectVO.FAILD);
+            resultObjectVO.setMsg("保存失败,类型名称不能为空");
+            return resultObjectVO;
+        }
+        if(StringUtils.isEmpty(messageTypeVO.getCode()))
+        {
+            resultObjectVO.setCode(ResultObjectVO.FAILD);
+            resultObjectVO.setMsg("保存失败,类型编码不能为空");
+            return resultObjectVO;
+        }
+        if(StringUtils.isEmpty(messageTypeVO.getAppCode()))
+        {
+            resultObjectVO.setCode(ResultObjectVO.FAILD);
+            resultObjectVO.setMsg("保存失败,所属应用不能为空");
+            return resultObjectVO;
+        }
+        String lockKey = messageTypeVO.getAppCode()+"_"+messageTypeVO.getCode();
+        try {
+            boolean lockStatus = skylarkLock.lock(MessageTypeKey.getSaveLockKey(lockKey), lockKey);
+            if (!lockStatus) {
+                resultObjectVO.setCode(ResultObjectVO.FAILD);
+                resultObjectVO.setMsg("保存失败,请稍后重试");
+                return resultObjectVO;
+            }
+
+            messageTypeVO.setId(idGenerator.id());
+            messageTypeVO.setDeleteStatus((short)0);
+            int ret = messageTypeService.save(messageTypeVO);
+            if(ret<=0)
+            {
+                logger.warn("保存收货信息失败 requestJson{} id{}",requestJsonVO.getEntityJson(),messageTypeVO.getId());
+                resultObjectVO.setCode(ResultVO.FAILD);
+                resultObjectVO.setMsg("保存失败,请稍后重试");
+            }
+            resultObjectVO.setData(messageTypeVO);
+        }catch(Exception e)
+        {
+            logger.warn(e.getMessage(),e);
+            resultObjectVO.setCode(ResultVO.FAILD);
+            resultObjectVO.setMsg("保存失败,请稍后重试");
+        }finally{
+            skylarkLock.unLock(MessageTypeKey.getSaveLockKey(lockKey), lockKey);
+        }
+        return resultObjectVO;
+    }
+
+
+
+
+
+
+
+
+
+}
