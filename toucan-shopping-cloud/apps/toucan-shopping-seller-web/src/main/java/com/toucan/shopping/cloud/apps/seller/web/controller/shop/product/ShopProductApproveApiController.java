439aa58304c00199933c89200df1f5f3f8250eb0
majian <mmdrss@163.com>
2022-04-08 16:04:28 +0800

    feat:卖家中心 商品审核列表

---
+++ toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/controller/shop/product/ShopProductApproveApiController.java
@@ -0,0 +1,115 @@
+package com.toucan.shopping.cloud.apps.seller.web.controller.shop.product;
+
+import com.alibaba.fastjson.JSONObject;
+import com.toucan.shopping.cloud.apps.seller.web.controller.BaseController;
+import com.toucan.shopping.cloud.apps.seller.web.redis.ShopProductRedisKey;
+import com.toucan.shopping.cloud.apps.seller.web.util.VCodeUtil;
+import com.toucan.shopping.cloud.apps.seller.web.vo.selectPage.GridResult;
+import com.toucan.shopping.cloud.product.api.feign.service.FeignAttributeKeyValueService;
+import com.toucan.shopping.cloud.product.api.feign.service.FeignShopProductApproveService;
+import com.toucan.shopping.cloud.seller.api.feign.service.FeignSellerShopService;
+import com.toucan.shopping.modules.auth.user.UserAuth;
+import com.toucan.shopping.modules.common.generator.RequestJsonVOGenerator;
+import com.toucan.shopping.modules.common.page.PageInfo;
+import com.toucan.shopping.modules.common.properties.Toucan;
+import com.toucan.shopping.modules.common.util.GlobalUUID;
+import com.toucan.shopping.modules.common.util.ImageUtils;
+import com.toucan.shopping.modules.common.util.UserAuthHeaderUtil;
+import com.toucan.shopping.modules.common.util.VerifyCodeUtil;
+import com.toucan.shopping.modules.common.vo.RequestJsonVO;
+import com.toucan.shopping.modules.common.vo.ResultObjectVO;
+import com.toucan.shopping.modules.common.vo.ResultVO;
+import com.toucan.shopping.modules.image.upload.service.ImageUploadService;
+import com.toucan.shopping.modules.product.page.ShopProductApprovePageInfo;
+import com.toucan.shopping.modules.product.vo.AttributeKeyVO;
+import com.toucan.shopping.modules.product.vo.ProductSkuVO;
+import com.toucan.shopping.modules.product.vo.PublishProductVO;
+import com.toucan.shopping.modules.redis.service.ToucanStringRedisService;
+import com.toucan.shopping.modules.seller.entity.SellerShop;
+import com.toucan.shopping.modules.seller.vo.SellerShopVO;
+import com.toucan.shopping.modules.user.vo.UserVO;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * 店铺商品审核信息
+ */
+@Controller("shopProductApproveApiController")
+@RequestMapping("/api/shop/product/approve")
+public class ShopProductApproveApiController extends BaseController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Toucan toucan;
+
+    @Autowired
+    private FeignShopProductApproveService feignShopProductApproveService;
+
+    @Autowired
+    private FeignSellerShopService feignSellerShopService;
+
+
+
+    /**
+     * 查询列表
+     * @param pageInfo
+     * @return
+     */
+    @UserAuth
+    @RequestMapping(value = "/list",method = RequestMethod.POST)
+    @ResponseBody
+    public PageInfo list(HttpServletRequest httpServletRequest,@RequestBody ShopProductApprovePageInfo pageInfo)
+    {
+        PageInfo pageResult = new PageInfo();
+        try{
+            if(pageInfo==null)
+            {
+                pageInfo = new ShopProductApprovePageInfo();
+            }
+            String userMainId = UserAuthHeaderUtil.getUserMainId(httpServletRequest.getHeader(toucan.getUserAuth().getHttpToucanAuthHeader()));
+            if(StringUtils.isEmpty(userMainId))
+            {
+                return pageResult;
+            }
+
+            SellerShop querySellerShop = new SellerShop();
+            querySellerShop.setUserMainId(Long.parseLong(userMainId));
+            RequestJsonVO requestJsonVO = RequestJsonVOGenerator.generator(this.getAppCode(), querySellerShop);
+            ResultObjectVO resultObjectVO = feignSellerShopService.findByUser(requestJsonVO.sign(),requestJsonVO);
+            if(resultObjectVO.isSuccess()&&resultObjectVO.getData()!=null) {
+                SellerShopVO sellerShopVO = resultObjectVO.formatData(SellerShopVO.class);
+                if(sellerShopVO!=null) {
+                    pageInfo.setShopId(sellerShopVO.getId());
+                    requestJsonVO = RequestJsonVOGenerator.generator(this.getAppCode(), pageInfo);
+                    resultObjectVO = feignShopProductApproveService.queryListPage(requestJsonVO);
+                    if(resultObjectVO.isSuccess()&&resultObjectVO.getData()!=null)
+                    {
+                        pageResult = resultObjectVO.formatData(PageInfo.class);
+                    }
+                }
+            }
+        }catch(Exception e)
+        {
+            logger.warn(e.getMessage(),e);
+        }
+
+        return pageResult;
+    }
+
+}
