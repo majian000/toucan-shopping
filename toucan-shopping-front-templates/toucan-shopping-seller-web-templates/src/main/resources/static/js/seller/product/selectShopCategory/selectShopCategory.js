5dae000203e09e888b3bfa9292d1b1a57363cea1
majian <mmdrss@163.com>
2022-05-03 11:33:26 +0800

    feat:重新发布商品 可修改商品分类

--- toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/product/selectShopCategory/selectShopCategory.js
+++ toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/product/selectShopCategory/selectShopCategory.js
@@ -14,7 +14,7 @@ var arrow = " <font>&gt;</font> ";
 function intRootShopCategory() {
 	areaShopCategoryCont = "";
 	for (var i=0; i<shop_category_list.length; i++) {
-		areaShopCategoryCont += '<li onClick="selectShopCategoryLevel1(' + i + '); " id="shop_category_'+category_list[i].id+'"><a href="javascript:void(0)">' + shop_category_list[i].name + '</a></li>';
+		areaShopCategoryCont += "<li onClick=\"selectShopCategoryLevel1(" + i + "); \" id=\"shop_category_"+shop_category_list[i].id+"\"><a href=\"javascript:void(0)\">" + shop_category_list[i].name + "</a></li>";
 	}
 	$("#shop_category_sort1").html(areaShopCategoryCont);
 }
@@ -24,7 +24,7 @@ function intRootShopCategory() {
 function selectShopCategoryLevel1(p) {
 	areaShopCategoryCont = "";
 	for (var j=0; j<shop_category_list[p].children.length; j++) {
-		areaShopCategoryCont += '<li onClick="selectShopCategoryLevel2(' + p + ',' + j + ');" id="shop_category_'+shop_category_list[p].children[j].id+'"><a href="javascript:void(0)">' + shop_category_list[p].children[j].name + '</a></li>';
+		areaShopCategoryCont += "<li onClick=\"selectShopCategoryLevel2(" + p + "," + j + ");\" id=\"shop_category_"+shop_category_list[p].children[j].id+"\"><a href=\"javascript:void(0)\">" + shop_category_list[p].children[j].name + "</a></li>";
 	}
 	$("#shop_category_sort2").html(areaShopCategoryCont).show();
 	if($("#shop_category_sort1 li").eq(p).hasClass("active"))
