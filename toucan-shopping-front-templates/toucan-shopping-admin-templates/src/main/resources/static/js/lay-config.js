d46f2a2fb6393861ff746f0b9a2864f16b2ef9d6
majian <mmdrss@163.com>
2022-06-28 21:21:28 +0800

    优化图表等功能

--- toucan-shopping-front-templates/toucan-shopping-admin-templates/src/main/resources/static/js/lay-config.js
+++ toucan-shopping-front-templates/toucan-shopping-admin-templates/src/main/resources/static/js/lay-config.js
@@ -23,9 +23,9 @@ layui.config({
     treetable: 'treetable-lay/treetable', //table树形扩展
     tableSelect: 'tableSelect/tableSelect', // table选择扩展
     iconPickerFa: 'iconPicker/iconPickerFa', // fa图标选择扩展
-    // echarts: 'echarts/echarts', // echarts图表扩展
+    echarts: 'echarts/echarts', // echarts图表扩展
     echartsTheme: 'echarts/echartsTheme', // echarts图表主题扩展
     wangEditor: 'wangEditor/wangEditor', // wangEditor富文本扩展
     layarea: 'layarea/layarea', //  省市县区三级联动下拉选择器
-    echarts: 'echarts-5/echarts', //echarts5
+    // echarts: 'echarts-5/echarts', //echarts5
 });
