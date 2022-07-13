a8bb529ed164adb4260db49b1f3baee41b5c2fcb
majian <mmdrss@163.com>
2022-07-13 11:37:26 +0800

    完善代码

--- toucan-shopping-cloud/services/toucan-shopping-admin-auth/src/main/java/com/toucan/shopping/cloud/admin/auth/app/config/ThreadPoolTaskConfig.java
+++ toucan-shopping-cloud/services/toucan-shopping-admin-auth/src/main/java/com/toucan/shopping/cloud/admin/auth/app/config/ThreadPoolTaskConfig.java
@@ -61,16 +61,12 @@ class ThreadContextDecorator implements TaskDecorator {
     @Nonnull
     public Runnable decorate(@Nonnull Runnable runnable) {
 
-        // 获取主线程中的请求信息（我们的用户信息也放在里面）
         RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
         return () -> {
             try {
-                // 将主线程的请求信息，设置到子线程中
                 RequestContextHolder.setRequestAttributes(attributes);
-                // 执行子线程，这一步不要忘了
                 runnable.run();
             } finally {
-                // 线程结束，清空这些信息，否则可能造成内存泄漏
                 RequestContextHolder.resetRequestAttributes();
             }
         };
