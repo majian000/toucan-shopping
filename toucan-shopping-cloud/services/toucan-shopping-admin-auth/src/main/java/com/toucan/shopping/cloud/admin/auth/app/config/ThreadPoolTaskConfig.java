2cbb338044973b5119dba7fd61bac5d55ce5537b
majian <mmdrss@163.com>
2022-07-13 11:37:14 +0800

    完善代码

--- toucan-shopping-cloud/services/toucan-shopping-admin-auth/src/main/java/com/toucan/shopping/cloud/admin/auth/app/config/ThreadPoolTaskConfig.java
+++ toucan-shopping-cloud/services/toucan-shopping-admin-auth/src/main/java/com/toucan/shopping/cloud/admin/auth/app/config/ThreadPoolTaskConfig.java
@@ -5,10 +5,14 @@ import com.toucan.shopping.modules.admin.auth.constant.LoginHistoryAsyncConstant
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
+import javax.annotation.Nonnull;
 import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
@@ -41,8 +45,34 @@ public class ThreadPoolTaskConfig {
         executor.setKeepAliveSeconds(keepAliveSeconds);
         executor.setThreadNamePrefix(threadNamePrefix);
 
+        executor.setTaskDecorator(new ThreadContextDecorator());
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
+    }
+}
+
+
+/**
+ * 保存请求上下文到子线程中
+ */
+class ThreadContextDecorator implements TaskDecorator {
+    @Override
+    @Nonnull
+    public Runnable decorate(@Nonnull Runnable runnable) {
+
+        // 获取主线程中的请求信息（我们的用户信息也放在里面）
+        RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
+        return () -> {
+            try {
+                // 将主线程的请求信息，设置到子线程中
+                RequestContextHolder.setRequestAttributes(attributes);
+                // 执行子线程，这一步不要忘了
+                runnable.run();
+            } finally {
+                // 线程结束，清空这些信息，否则可能造成内存泄漏
+                RequestContextHolder.resetRequestAttributes();
+            }
+        };
     }
 }
