c4eb829c947cc802b63c7231337c192ca2ef96fe
majian <mmdrss@163.com>
2022-07-23 10:36:19 +0800

    feat:栏目管理

--- toucan-shopping-modules/column/toucan-shopping-column-entity-vo/src/main/java/com/toucan/shopping/modules/column/entity/ColumnRecommendLabel.java
+++ toucan-shopping-modules/column/toucan-shopping-column-entity-vo/src/main/java/com/toucan/shopping/modules/column/entity/ColumnRecommendLabel.java
@@ -5,24 +5,23 @@ import lombok.Data;
 import java.util.Date;
 
 /**
- * 栏目
+ * 栏目推荐标签
  *
  * @author majian
  */
 @Data
-public class Column {
+public class ColumnRecommendLabel {
     private Long id; //主键
-    private String title; //编码
-    private Integer type; //类型 1:pc端 2:移动端
-    private Integer showStatus; //显示状态 0隐藏 1显示
-    private Integer position; //栏目位置 1 PC门户首页
+    private Long columnId; //栏目主键
+    private String labelName; //标签名称
+    private String clickPath; //点击路径
+    private Short position; //位置 1顶部 2左侧
     private Date createDate; //创建时间
     private Date updateDate; //创建时间
     private String appCode; //所属应用
     private String createAdminId; //创建人ID
     private String updateAdminId; //修改人ID
     private String remark; //备注
-    private Integer columnSort; //排序
 
     /**
      * 删除状态 0未删除 1已删除
