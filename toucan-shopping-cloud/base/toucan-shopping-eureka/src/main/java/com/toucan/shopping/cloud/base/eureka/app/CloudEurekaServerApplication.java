0def98ed8cd26f25bbec1a017e6dd5c8138cedba
majian <mmdrss@163.com>
2022-07-26 14:43:57 +0800

    服务注册中心 升级成nacos

--- toucan-shopping-cloud/base/toucan-shopping-eureka/src/main/java/com/toucan/shopping/cloud/base/eureka/app/CloudEurekaServerApplication.java
+++ toucan-shopping-cloud/base/toucan-shopping-eureka/src/main/java/com/toucan/shopping/cloud/base/eureka/app/CloudEurekaServerApplication.java
@@ -1,13 +1,16 @@
 package com.toucan.shopping.cloud.base.eureka.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@Slf4j
 @EnableEurekaServer
 @SpringBootApplication
 public class CloudEurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CloudEurekaServerApplication.class, args);
+      log.info("服务注册中心已经替换成nacos");
+//        SpringApplication.run(CloudEurekaServerApplication.class, args);
     }
 }
