0f99d13ebd31293f5030c8986766a0817b061d22
majian <mmdrss@163.com>
2022-07-29 19:04:17 +0800

    feat:选择商品组件

--- toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/component/SelectShopProductController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/component/SelectShopProductController.java
@@ -104,13 +104,14 @@ public class SelectShopProductController extends UIController {
 
 
     @AdminAuth(verifyMethod = AdminAuth.VERIFYMETHOD_ADMIN_AUTH,requestType = AdminAuth.REQUEST_FORM)
-    @RequestMapping(value = "/shopProductListPage/{categoryId}",method = RequestMethod.GET)
-    public String spuListPage(HttpServletRequest request,@PathVariable Long categoryId)
+    @RequestMapping(value = "/shopProductListPage",method = RequestMethod.GET)
+    public String spuListPage(HttpServletRequest request,@RequestParam Long categoryId,@RequestParam String selectProductIds)
     {
         //初始化工具条按钮、操作按钮
         super.initButtons(request,toucan,"/component/selectShopProduct/shopProductListPage",feignFunctionService);
 
         request.setAttribute("categoryId",categoryId);
+        request.setAttribute("selectProductIds",selectProductIds);
         return "pages/component/selectShopProduct/shop_product_list.html";
     }
 
