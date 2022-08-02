0f4047d15bf7d1d7bf9a0fc8a8bae0cb2f8bf898
majian <mmdrss@163.com>
2022-08-02 09:06:50 +0800

    增加响应400防止熔断处理

---
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/app/config/BizExceptionFeignErrorDecoder.java
@@ -0,0 +1,21 @@
+package com.toucan.shopping.cloud.apps.admin.app.config;
+
+import com.netflix.hystrix.exception.HystrixBadRequestException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 防止400触发熔断
+ */
+@Configuration
+public class BizExceptionFeignErrorDecoder implements ErrorDecoder {
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        if(response.status() >= 400 && response.status() <= 499){
+            return new HystrixBadRequestException("服务调用失败 请求服务状态码为 "+response.status()+" ");
+        }
+        return feign.FeignException.errorStatus(methodKey, response);
+    }
+}
