e051f45aec933fe5f3c4e05949e21877740a4f73
majian <mmdrss@163.com>
2022-08-03 15:57:12 +0800

    PC首页栏目 添加轮播图

--- toucan-shopping-modules/column/toucan-shopping-column-entity-vo/src/main/java/com/toucan/shopping/modules/column/vo/PcIndexColumnVO.java
+++ toucan-shopping-modules/column/toucan-shopping-column-entity-vo/src/main/java/com/toucan/shopping/modules/column/vo/PcIndexColumnVO.java
@@ -2,13 +2,18 @@ package com.toucan.shopping.modules.column.vo;
 
 import com.toucan.shopping.modules.column.entity.ColumnRecommendLabel;
 import com.toucan.shopping.modules.column.vo.ColumnVO;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class PcIndexColumnVO extends ColumnVO {
 
     private List<ColumnRecommendLabel> topLabels; //顶部标签
 
     private List<ColumnRecommendLabel> leftLabels; //左侧标签
+
+    private List<ColumnBannerVO> columnLeftBannerVOS; //栏目左侧轮播图
+
 
 }
