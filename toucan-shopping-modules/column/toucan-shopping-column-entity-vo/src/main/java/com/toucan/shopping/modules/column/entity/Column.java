778698d1ab4d585eb965f756737de59000de1b2a
majian <mmdrss@163.com>
2022-08-04 17:57:18 +0800

    feat:首页栏目列表

--- toucan-shopping-modules/column/toucan-shopping-column-entity-vo/src/main/java/com/toucan/shopping/modules/column/entity/Column.java
+++ toucan-shopping-modules/column/toucan-shopping-column-entity-vo/src/main/java/com/toucan/shopping/modules/column/entity/Column.java
@@ -18,7 +18,15 @@ public class Column {
     private Integer type; //类型 1:pc端 2:移动端
     private Integer showStatus; //显示状态 0隐藏 1显示
     private Integer position; //栏目位置 1 PC门户首页
+
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createDate; //创建时间
+
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateDate; //创建时间
     private String appCode; //所属应用
     private String createAdminId; //创建人ID
