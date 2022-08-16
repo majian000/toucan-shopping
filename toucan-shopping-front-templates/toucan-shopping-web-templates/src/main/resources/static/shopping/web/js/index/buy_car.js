c346f21ea65be683b5369a5b0430dcd446bf96f4
majian <mmdrss@163.com>
2022-08-16 11:25:39 +0800

    feat:C端购物车功能

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/buy_car.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/buy_car.js
@@ -2,10 +2,13 @@ $(function () {
 
     //查询信息
     $.post(basePath + "/api/user/buyCar/preview/info", {}, function (result) {
-        if (result.code == 403) {
-            $(".buy_car_login_tip").show();
-        }else if(result.code == 0){
+        $(".buy_car_login_tip").hide();
+        if(result.code == 1){
 
+            if(result.data == null||result.data.productCount==0)
+            {
+                $(".buy_car_empty").show();
+            }
         }
     });
 
