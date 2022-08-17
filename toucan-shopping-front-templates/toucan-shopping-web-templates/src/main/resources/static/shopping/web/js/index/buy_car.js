61f6ffe974cace35f97bd9d41a842e20434a86c4
majian <mmdrss@163.com>
2022-08-17 15:38:17 +0800

    feat:C端购物车

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/buy_car.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/buy_car.js
@@ -4,10 +4,18 @@ $(function () {
     $.post(basePath + "/api/user/buyCar/preview/info", {}, function (result) {
         $(".buy_car_login_tip").hide();
         if(result.code == 1){
+            var productCount = 0;
+            if(result.data!=null&&result.data.length>0)
+            {
+                productCount = result.data.length;
+            }
+            $("#buyCarProductCount").html(productCount);
 
-            if(result.data == null|| result.data.length<=0 || result.data.productCount==0)
+            if(result.data == null|| result.data.length<=0)
             {
                 $(".buy_car_empty").show();
+            }else{
+
             }
         }
     });
