f1edff49f652f54346c83e944c03e7d50f29ea26
majian <mmdrss@163.com>
2022-08-17 17:15:32 +0800

    feat:C端购物车

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/buy_car.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/buy_car.js
@@ -15,7 +15,22 @@ $(function () {
             {
                 $(".buy_car_empty").show();
             }else{
+                var productHtmls="";
+                var productPriceTotal = 0;
+                for(var i=0;i<result.data.length;i++)
+                {
+                    productHtmls+="<li>\n" +
+                        "                <div class=\"img\"><a href=\"#\"><img src=\""+basePath+"/static/images/car1.jpg\" width=\"58\" height=\"58\" /></a></div>\n" +
+                        "                <div class=\"name\"><a href=\"#\">法颂浪漫梦境50ML 香水女士持久清新淡香 送2ML小样3只</a></div>\n" +
+                        "                <div class=\"price\"><font color=\"#ff4e00\">￥399</font> X1</div>\n" +
+                        "            </li>";
+                }
+                $(".cars").html(productHtmls);
+                $("#mcars_price_total_label").val(productPriceTotal);
 
+                $(".mcars").show();
+                $(".mcars_price_total").show();
+                $(".mcars_pay").show();
             }
         }
     });
