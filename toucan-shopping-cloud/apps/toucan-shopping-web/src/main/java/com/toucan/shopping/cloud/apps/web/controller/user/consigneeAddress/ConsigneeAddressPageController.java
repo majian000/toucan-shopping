834d8195670a761bf36f812472024ab216dabf77
majian <mmdrss@163.com>
2022-08-25 17:36:21 +0800

    收货地址管理

--- toucan-shopping-cloud/apps/toucan-shopping-web/src/main/java/com/toucan/shopping/cloud/apps/web/controller/user/consigneeAddress/ConsigneeAddressPageController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-web/src/main/java/com/toucan/shopping/cloud/apps/web/controller/user/consigneeAddress/ConsigneeAddressPageController.java
@@ -1,9 +1,13 @@
 package com.toucan.shopping.cloud.apps.web.controller.user.consigneeAddress;
 
 
+import com.toucan.shopping.cloud.apps.web.service.LoginUserService;
 import com.toucan.shopping.modules.auth.user.UserAuth;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 用户收货地址
@@ -15,7 +19,7 @@ public class ConsigneeAddressPageController {
 
     @UserAuth
     @RequestMapping("/add")
-    public String indexPage()
+    public String addPage(HttpServletRequest request)
     {
         return "user/consigneeAddress/add";
     }
---
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/user/consigneeAddress/add.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#ms_city").click(function (e) {
+        SelCity(this,e);
+    });
+    $(".msc_l").click(function (e) {
+        SelCity(document.getElementById("ms_city"),e);
+    });
+
+});
