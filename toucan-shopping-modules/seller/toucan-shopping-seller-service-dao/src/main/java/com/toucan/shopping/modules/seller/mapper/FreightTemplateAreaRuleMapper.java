f4155c9f207b1582b89a53cbf57deb82a1c9bcae
majian <mmdrss@163.com>
2022-09-20 11:20:17 +0800

    运费模板 service、dao

---
+++ toucan-shopping-modules/seller/toucan-shopping-seller-service-dao/src/main/java/com/toucan/shopping/modules/seller/mapper/FreightTemplateAreaRuleMapper.java
@@ -0,0 +1,9 @@
+package com.toucan.shopping.modules.seller.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface FreightTemplateAreaRuleMapper {
+
+}
---
+++ toucan-shopping-modules/seller/toucan-shopping-seller-service-dao/src/main/java/com/toucan/shopping/modules/seller/mapper/FreightTemplateMapper.java
@@ -0,0 +1,14 @@
+package com.toucan.shopping.modules.seller.mapper;
+
+import com.toucan.shopping.modules.seller.entity.SellerShop;
+import com.toucan.shopping.modules.seller.page.SellerShopPageInfo;
+import com.toucan.shopping.modules.seller.vo.SellerShopVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+@Mapper
+public interface FreightTemplateMapper {
+
+}
