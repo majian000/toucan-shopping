cb7fda40e854ffa7292f778363ca9204a8a90d90
majian <mmdrss@163.com>
2022-12-19 16:28:58 +0800

    定时任务 调度中心

--- toucan-shopping-cloud/apps/toucan-shopping-scheduler/src/main/java/com/toucan/shopping/cloud/apps/scheduler/app/CloudSchedulerApplication.java
+++ toucan-shopping-cloud/apps/toucan-shopping-scheduler/src/main/java/com/toucan/shopping/cloud/apps/scheduler/app/CloudSchedulerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.toucan.shopping")
 @ComponentScan("com.toucan.shopping")
