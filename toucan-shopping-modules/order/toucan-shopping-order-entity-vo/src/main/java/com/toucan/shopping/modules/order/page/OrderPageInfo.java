76af8c5799c3f55ef6e906de98c2f7f7cfe0b3b6
majian <mmdrss@163.com>
2023-01-10 16:10:45 +0800

    后台管理端 订单列表

--- toucan-shopping-modules/order/toucan-shopping-order-entity-vo/src/main/java/com/toucan/shopping/modules/order/page/OrderPageInfo.java
+++ toucan-shopping-modules/order/toucan-shopping-order-entity-vo/src/main/java/com/toucan/shopping/modules/order/page/OrderPageInfo.java
@@ -1,5 +1,6 @@
 package com.toucan.shopping.modules.order.page;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.toucan.shopping.modules.common.page.PageInfo;
 import com.toucan.shopping.modules.order.entity.Order;
 import com.toucan.shopping.modules.order.vo.OrderVO;
@@ -9,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.List;
@@ -34,10 +36,22 @@ public class OrderPageInfo extends PageInfo<OrderVO> {
     private String appCode; //所属应用
 
     private String userId; //所属用户
+    private String shopId; //所属店铺
     private String orderNo; //订单编号
 
-    private Integer payStatus; //支付状态 0未支付 1已支付 3线下支付已到账 4取消支付
+    private Integer payStatus; //支付状态 -1全部 0未支付 1已支付 3线下支付已到账 4取消支付
     private Integer tradeStatus; //交易状态 0:待付款 1:待收货 2:已取消 3:已完成
+
+
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date startCreateDate; //开始下单时间
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date endCreateDate; //结束下单时间
+
 
     //==============================================
 
