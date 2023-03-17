d5e8d0b1abc4b250b35106fae505279e707b88f3
majian <mmdrss@163.com>
2023-03-17 10:31:24 +0800

    异常邮件增加忽略请求头异常

---
+++ toucan-shopping-modules/common/toucan-shopping-common/src/main/java/com/toucan/shopping/modules/common/exception/InvalidAdminHeaderException.java
@@ -0,0 +1,7 @@
+package com.toucan.shopping.modules.common.exception;
+
+public class InvalidAdminHeaderException extends Exception {
+    public InvalidAdminHeaderException(String message) {
+        super(message);
+    }
+}
