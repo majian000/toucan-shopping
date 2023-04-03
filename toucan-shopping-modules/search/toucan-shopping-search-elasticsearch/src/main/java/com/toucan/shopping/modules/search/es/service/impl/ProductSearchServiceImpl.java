760491263e9ce2b074ad8620684c27bfbbf115fa
majian <mmdrss@163.com>
2023-04-03 16:03:33 +0800

    搜索模块

--- toucan-shopping-modules/search/toucan-shopping-search-elasticsearch/src/main/java/com/toucan/shopping/modules/search/es/service/impl/ProductSearchServiceImpl.java
+++ toucan-shopping-modules/search/toucan-shopping-search-elasticsearch/src/main/java/com/toucan/shopping/modules/search/es/service/impl/ProductSearchServiceImpl.java
@@ -1,10 +1,34 @@
 package com.toucan.shopping.modules.search.es.service.impl;
 
+import com.toucan.shopping.modules.search.es.index.ProductIndex;
 import com.toucan.shopping.modules.search.es.service.ProductSearchService;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service("productSearchServiceImpl")
 public class ProductSearchServiceImpl implements ProductSearchService {
+
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
     @Override
     public void createIndex() {
-
+        try {
+            CreateIndexRequest request = new CreateIndexRequest(ProductIndex.PRODUCT_INDEX);
+            restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+        }catch(Exception e)
+        {
+            logger.warn(e.getMessage(),e);
+        }
     }
 }
