32f16cde8c9cc1eaece91e1e004ac5dcac3c9a07
majian <mmdrss@163.com>
2023-04-03 15:15:54 +0800

    搜索模块

---
+++ toucan-shopping-modules/search/toucan-shopping-search-elasticsearch/src/main/java/com/toucan/shopping/modules/search/es/service/impl/ProductSearchServiceImpl.java
@@ -0,0 +1,10 @@
+package com.toucan.shopping.modules.search.es.service.impl;
+
+import com.toucan.shopping.modules.search.es.service.ProductSearchService;
+
+public class ProductSearchServiceImpl implements ProductSearchService {
+    @Override
+    public void createIndex() {
+
+    }
+}
---
+++ toucan-shopping-modules/search/toucan-shopping-search/src/main/java/com/toucan/shopping/modules/search/es/service/ProductSearchService.java
@@ -0,0 +1,13 @@
+package com.toucan.shopping.modules.search.es.service;
+
+/**
+ * 商品搜索
+ */
+public interface ProductSearchService {
+
+    /**
+     * 创建索引
+     */
+    void createIndex();
+
+}
