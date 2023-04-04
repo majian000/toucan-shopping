b4eb1b5cd45501fc16c339ca3f859f4b760a375b
majian <mmdrss@163.com>
2023-04-04 17:25:26 +0800

    搜索服务

--- toucan-shopping-cloud/services/toucan-shopping-search/src/main/java/com/toucan/shopping/cloud/search/app/CloudSearchServiceApplication.java
+++ toucan-shopping-cloud/services/toucan-shopping-search/src/main/java/com/toucan/shopping/cloud/search/app/CloudSearchServiceApplication.java
@@ -1,7 +1,5 @@
 package com.toucan.shopping.cloud.search.app;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
