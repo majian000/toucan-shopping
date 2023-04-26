7a507fc40a133f7ad5e3442a8869d6227f96cbe8
majian <mmdrss@163.com>
2023-04-26 17:23:00 +0800

    修复bug

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/search/product/search.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/search/product/search.js
@@ -4,8 +4,8 @@ $(function(){
 
     $(".s_btn").bind("click", function () {
         var sq=$(".s_ipt").val();
-        window.open(basePath+"/api/product/search?keyword="+sq);
         window.close();
+        window.open(basePath+"/api/product/search?keyword="+sq);
     });
 
 });
