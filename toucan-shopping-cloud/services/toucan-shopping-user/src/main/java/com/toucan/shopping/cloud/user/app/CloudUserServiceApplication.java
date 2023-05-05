b527bea7e17c6a144ca735ade41726fded58c731
majian <mmdrss@163.com>
2023-05-05 10:20:22 +0800

    拆分微服务控制器 到模块层

--- toucan-shopping-cloud/services/toucan-shopping-user/src/main/java/com/toucan/shopping/cloud/user/app/CloudUserServiceApplication.java
+++ toucan-shopping-cloud/services/toucan-shopping-user/src/main/java/com/toucan/shopping/cloud/user/app/CloudUserServiceApplication.java
@@ -1,18 +1,22 @@
 package com.toucan.shopping.cloud.user.app;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.toucan.shopping.modules.kafka.config.KafkaCustomerConfig;
+import com.toucan.shopping.modules.kafka.config.KafkaProducerConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 @EnableDiscoveryClient
 @MapperScan({"com.toucan.shopping.modules.user.mapper",
         "com.toucan.shopping.modules.common.persistence.mapper"})
-@ComponentScans({@ComponentScan("com.toucan.shopping")})
+@ComponentScan(basePackages = {"com.toucan.shopping"},
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = KafkaCustomerConfig.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = KafkaProducerConfig.class)})
 public class CloudUserServiceApplication {
 
     public static void main(String[] args) {
