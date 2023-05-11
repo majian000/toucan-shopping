f753a09d44d1e8bdc01435b27e12bc1802a93562
majian <mmdrss@163.com>
2023-05-11 14:42:59 +0800

    按照分类搜索商品

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/index.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/index/index.js
@@ -9,4 +9,16 @@ $(function(){
            window.location.href=href;
        }
     });
+
+    $(".category_a").bind("click", function () {
+        var href = $(this).attr("href");
+        if(href==null||href==""||href=="#"||href=="javascript:void(0)")
+        {
+            window.location.href=searchGetPath+"?cid="+$(this).attr("attr-id");
+        }else{
+            window.location.href=href;
+        }
+    });
+
+
 });
