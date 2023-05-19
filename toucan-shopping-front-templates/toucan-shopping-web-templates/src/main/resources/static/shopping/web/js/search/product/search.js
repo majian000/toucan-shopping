d242b6f4f5983556fb9f2d79770e7b6fb2e6e2a4
majian <mmdrss@163.com>
2023-05-19 16:43:08 +0800

    搜索增加分类过滤条件

--- toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/search/product/search.js
+++ toucan-shopping-front-templates/toucan-shopping-web-templates/src/main/resources/static/shopping/web/js/search/product/search.js
@@ -5,7 +5,8 @@ $(function(){
     $(".s_btn").bind("click", function () {
         // $(".s_form").attr("action","/api/product/g/search");
         // $(".s_form").submit();
-        window.location.href=searchGetPath+"?keyword="+$(".s_ipt").val();
+        var cid=$("#cid").val();
+        window.location.href=searchGetPath+"?keyword="+$(".s_ipt").val()+"&cid="+cid;
     });
 
     $(".q_labels").bind("click", function () {
