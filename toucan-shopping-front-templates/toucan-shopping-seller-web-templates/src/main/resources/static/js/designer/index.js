d6f6a9c750a47886c6e657fc240a0e76d26d91ac
majian <mmdrss@163.com>
2023-09-23 15:29:29 +0800

    卖家中心设计器 调整属性样式

---
+++ toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/designer/index.js
@@ -0,0 +1,20 @@
+$(function () {
+    initDesignerProperty();
+});
+
+
+function initDesignerProperty()
+{
+    $(".mtc1").show();
+
+    $(".mt-tabpage-item").click(function(){
+        var tabTitles = $(".mt-tabpage-item");
+        for(var i=0;i<tabTitles.length;i++)
+        {
+            $(tabTitles[i]).removeClass("mt-tabpage-item-cur");
+        }
+        $(this).addClass("mt-tabpage-item-cur");
+        $(".mt-tabpage-content").hide();
+        $("."+$(this).attr("attr-page")).show();
+    })
+}
