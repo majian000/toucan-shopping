72db005e7d38e9901d09c57f5514815ba257c377
majian <mmdrss@163.com>
2023-11-03 19:50:05 +0800

    设计器背景样式属性

--- toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/designer/pc/index.js
+++ toucan-shopping-front-templates/toucan-shopping-seller-web-templates/src/main/resources/static/js/seller/designer/pc/index.js
@@ -9,6 +9,7 @@ function initDesignerProperty()
 {
     $(".mtc1").show();
 
+    //监听属性选项卡
     $(".mt-tabpage-item").click(function(){
         var tabTitles = $(".mt-tabpage-item");
         for(var i=0;i<tabTitles.length;i++)
@@ -18,5 +19,17 @@ function initDesignerProperty()
         $(this).addClass("mt-tabpage-item-cur");
         $(".mt-tabpage-content").hide();
         $("."+$(this).attr("attr-page")).show();
-    })
+    });
+
+    //背景样式
+    $("#bgType1").click(function(){
+        $(".bgTypeColor").show();
+        $(".bgTypeImg").hide();
+    });
+    $("#bgType2").click(function(){
+        $(".bgTypeImg").show();
+        $(".bgTypeColor").hide();
+    });
+
+
 }
