94e959362bf7703c02808c68884b15fedc25a26d
majian <mmdrss@163.com>
2024-04-02 18:02:34 +0800

    待收货订单

---
+++ toucan-shopping-cloud/apps/toucan-shopping-seller-web/src/main/java/com/toucan/shopping/cloud/apps/seller/web/controller/waitDeliveryOrder/WaitDeliveryOrderPageController.java
@@ -0,0 +1,53 @@
+package com.toucan.shopping.cloud.apps.seller.web.controller.waitDeliveryOrder;
+
+import com.toucan.shopping.cloud.apps.seller.web.controller.BaseController;
+import com.toucan.shopping.cloud.apps.seller.web.service.CategoryService;
+import com.toucan.shopping.cloud.seller.api.feign.service.FeignShopCategoryService;
+import com.toucan.shopping.modules.auth.user.UserAuth;
+import com.toucan.shopping.modules.common.properties.Toucan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * 待收货订单
+ */
+@Controller("waitDeliveryOrderPageController")
+@RequestMapping("/page/waitDeliveryOrder")
+public class WaitDeliveryOrderPageController extends BaseController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Toucan toucan;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private FeignShopCategoryService feignShopCategoryService;
+
+
+    @UserAuth(requestType = UserAuth.REQUEST_FORM,responseType = UserAuth.RESPONSE_FORM)
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request){
+
+        return "waitDeliveryOrder/index";
+    }
+
+    @UserAuth(requestType = UserAuth.REQUEST_FORM,responseType = UserAuth.RESPONSE_FORM)
+    @RequestMapping("/show/{id}")
+    public String show(HttpServletRequest request, @PathVariable String id ){
+        request.setAttribute("id",id);
+        return "waitDeliveryOrder/show";
+    }
+
+
+
+}
