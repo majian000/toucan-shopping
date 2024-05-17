5398ec8bec2caf6441f61ae3342a3f221478e2dd
majian <mmdrss@163.com>
2024-05-17 16:17:11 +0800

    栏目列表

--- toucan-shopping-cloud/modules/apps/toucan-shopping-cloud-admin-auth-controller/src/main/java/com/toucan/shopping/cloud/apps/admin/auth/web/controller/dict/DictCategoryController.java
+++ toucan-shopping-cloud/modules/apps/toucan-shopping-cloud-admin-auth-controller/src/main/java/com/toucan/shopping/cloud/apps/admin/auth/web/controller/dict/DictCategoryController.java
@@ -84,7 +84,7 @@ public class DictCategoryController extends UIController {
     {
         TableVO tableVO = new TableVO();
         try {
-            pageInfo.setAppCode(toucan.getShoppingPC().getAppCode());
+            pageInfo.setAppCode(toucan.getAppCode());
             RequestJsonVO requestJsonVO = RequestJsonVOGenerator.generator(toucan.getAppCode(),pageInfo);
             ResultObjectVO resultObjectVO = feignDictCategoryService.listPage(requestJsonVO);
             if(resultObjectVO.isSuccess())
@@ -164,7 +164,7 @@ public class DictCategoryController extends UIController {
     {
         ResultObjectVO resultObjectVO = new ResultObjectVO();
         try {
-            entity.setAppCode(toucan.getShoppingPC().getAppCode());
+            entity.setAppCode(toucan.getAppCode());
             entity.setCreateAdminId(AuthHeaderUtil.getAdminId(toucan.getAppCode(),request.getHeader(toucan.getAdminAuth().getHttpToucanAuthHeader())));
             RequestJsonVO requestJsonVO = RequestJsonVOGenerator.generator(appCode, entity);
             resultObjectVO = feignDictCategoryService.save(requestJsonVO);
@@ -225,7 +225,7 @@ public class DictCategoryController extends UIController {
     {
         ResultObjectVO resultObjectVO = new ResultObjectVO();
         try {
-            entity.setAppCode(toucan.getShoppingPC().getAppCode());
+            entity.setAppCode(toucan.getAppCode());
             entity.setUpdateAdminId(AuthHeaderUtil.getAdminId(toucan.getAppCode(),request.getHeader(toucan.getAdminAuth().getHttpToucanAuthHeader())));
             entity.setUpdateDate(new Date());
             RequestJsonVO requestJsonVO = RequestJsonVOGenerator.generator(appCode, entity);
