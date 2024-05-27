e57d31357d31ffa38d5173f558cb9d169203d330
majian <mmdrss@163.com>
2024-05-27 14:44:40 +0800

    商品库存锁定 增加类型查询条件

--- toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/stock/skuStockLock/ProductSkuStockLockController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/stock/skuStockLock/ProductSkuStockLockController.java
@@ -3,12 +3,14 @@ package com.toucan.shopping.cloud.apps.admin.controller.stock.skuStockLock;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.toucan.shopping.cloud.admin.auth.api.feign.service.FeignDictService;
 import com.toucan.shopping.cloud.admin.auth.api.feign.service.FeignFunctionService;
 import com.toucan.shopping.cloud.apps.admin.auth.web.controller.base.UIController;
 import com.toucan.shopping.cloud.common.data.api.feign.service.FeignCategoryService;
 import com.toucan.shopping.cloud.search.api.feign.service.FeignProductSearchService;
 import com.toucan.shopping.cloud.stock.api.feign.service.FeignProductSkuStockLockService;
 import com.toucan.shopping.modules.admin.auth.vo.AdminVO;
+import com.toucan.shopping.modules.admin.auth.vo.DictVO;
 import com.toucan.shopping.modules.auth.admin.AdminAuth;
 import com.toucan.shopping.modules.category.vo.CategoryTreeVO;
 import com.toucan.shopping.modules.category.vo.CategoryVO;
@@ -21,8 +23,10 @@ import com.toucan.shopping.modules.common.properties.Toucan;
 import com.toucan.shopping.modules.common.util.SignUtil;
 import com.toucan.shopping.modules.common.vo.RequestJsonVO;
 import com.toucan.shopping.modules.common.vo.ResultObjectVO;
+import com.toucan.shopping.modules.common.vo.ResultTypeObjectVO;
 import com.toucan.shopping.modules.image.upload.service.ImageUploadService;
 import com.toucan.shopping.modules.layui.vo.TableVO;
+import com.toucan.shopping.modules.product.constant.ProductStockLockDictConstant;
 import com.toucan.shopping.modules.search.vo.ProductSearchResultVO;
 import com.toucan.shopping.modules.search.vo.ProductSearchVO;
 import com.toucan.shopping.modules.stock.page.ProductSkuStockLockPageInfo;
@@ -37,6 +41,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -64,14 +69,40 @@ public class ProductSkuStockLockController extends UIController {
     @Autowired
     private FeignProductSkuStockLockService feignProductSkuStockLockService;
 
+    @Autowired
+    private FeignDictService feignDictService;
 
     @AdminAuth(verifyMethod = AdminAuth.VERIFYMETHOD_ADMIN_AUTH,requestType = AdminAuth.REQUEST_FORM,responseType=AdminAuth.RESPONSE_FORM)
     @RequestMapping(value = "/listPage",method = RequestMethod.GET)
-    public String listPage(HttpServletRequest request)
-    {
+    public String listPage(HttpServletRequest request) throws NoSuchAlgorithmException {
         //初始化工具条按钮、操作按钮
         super.initButtons(request,toucan,"/stock/productSkuStockLock/listPage",feignFunctionService);
+        this.setProductStockLockDictList(request);
         return "pages/stock/skuStockLock/list.html";
+    }
+
+
+
+    private void setProductStockLockDictList(HttpServletRequest request) throws NoSuchAlgorithmException {
+        DictVO queryDict=new DictVO();
+        queryDict.setCategoryCode(ProductStockLockDictConstant.PRODUCT_STOCK_LOCK_DICT_CATEGORY_CODE);
+        queryDict.setCodes(new LinkedList<>());
+        queryDict.getCodes().add(ProductStockLockDictConstant.PRODUCT_STOCK_LOCK_DICT_TYPE_CODE);
+        queryDict.setAppCode(toucan.getAppCode());
+        RequestJsonVO requestJsonVO = RequestJsonVOGenerator.generator(toucan.getAppCode(), queryDict);
+        ResultTypeObjectVO<List<DictVO>> resultObjectVO = feignDictService.queryDictByCodesAndCategoryCode(requestJsonVO);
+        if(resultObjectVO.isSuccess()) {
+            if(!CollectionUtils.isEmpty(resultObjectVO.getData())){
+                for(DictVO dictVO:resultObjectVO.getData()){
+                    switch (dictVO.getCode()){
+                        case ProductStockLockDictConstant.PRODUCT_STOCK_LOCK_DICT_TYPE_CODE:
+                            request.setAttribute("productStockLockTypeList",dictVO.getChildren());
+                            break;
+                    }
+                }
+            }
+        }
+
     }
 
 
---
+++ toucan-shopping-modules/product/toucan-shopping-product-entity-vo/src/main/java/com/toucan/shopping/modules/product/constant/ProductStockLockDictConstant.java
@@ -0,0 +1,9 @@
+package com.toucan.shopping.modules.product.constant;
+
+public class ProductStockLockDictConstant {
+
+    public static final String PRODUCT_STOCK_LOCK_DICT_CATEGORY_CODE="PRODUCT_STOCK_LOCK_DICT"; //商品锁定库存
+    public static final String PRODUCT_STOCK_LOCK_DICT_TYPE_CODE="PRODUCT_STOCK_LOCK_TYPE"; //锁定库存类型
+
+
+}
