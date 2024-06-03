7fafab110c166c2c6475db12bc220db5dcd67b94
majian <mmdrss@163.com>
2024-06-03 16:25:16 +0800

    调整分类 类型为多选

--- toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/category/CategoryController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/category/CategoryController.java
@@ -352,7 +352,7 @@ public class CategoryController extends UIController {
                                 for(int i=0;i<types.length;i++){
                                     String type = types[i];
                                     typeNames+=categoryTypeMap.get(type).getName();
-                                    if(i>0&&i<types.length){
+                                    if(i<types.length){
                                         typeNames+=",";
                                     }
                                 }
--- toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/column/ColumnController.java
+++ toucan-shopping-cloud/apps/toucan-shopping-admin/src/main/java/com/toucan/shopping/cloud/apps/admin/controller/column/ColumnController.java
@@ -359,7 +359,7 @@ public class ColumnController extends UIController {
                                     for (int i = 0; i < positions.length; i++) {
                                         String position = positions[i];
                                         positionNames += columnPositionMap.get(position).getName();
-                                        if (i > 0 &&i < positions.length) {
+                                        if (i < positions.length) {
                                             positionNames += ",";
                                         }
                                     }
