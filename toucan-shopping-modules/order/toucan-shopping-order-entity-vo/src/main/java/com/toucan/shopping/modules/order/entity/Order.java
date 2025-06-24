866ac8a7f928a1b4bbaa6866bf9b576d31653450
majian <mmdrss@163.com>
2025-06-24 16:51:55 +0800

    feat:订单增加已退款状态

--- toucan-shopping-modules/order/toucan-shopping-order-entity-vo/src/main/java/com/toucan/shopping/modules/order/entity/Order.java
+++ toucan-shopping-modules/order/toucan-shopping-order-entity-vo/src/main/java/com/toucan/shopping/modules/order/entity/Order.java
@@ -29,7 +29,7 @@ public class Order {
     private BigDecimal redPackageAmount = new BigDecimal(0); //红包金额
     private BigDecimal couponAmount = new BigDecimal(0); //优惠券金额
     private BigDecimal totalAmount; //订单金额(不算折扣)
-    private Integer payStatus; //支付状态 0未支付 1已支付  4取消支付
+    private Integer payStatus; //支付状态 0未支付 1已支付  4取消支付 5已退款
     private Integer tradeStatus; //交易状态 0:待付款 4:待发货 1:待收货 2:已取消 3:已完成
     private Integer payMethod;  //支付方式 1线上支付 2线下支付
     private Integer payType; //交易类型 -1未确定 0微信 1支付宝
