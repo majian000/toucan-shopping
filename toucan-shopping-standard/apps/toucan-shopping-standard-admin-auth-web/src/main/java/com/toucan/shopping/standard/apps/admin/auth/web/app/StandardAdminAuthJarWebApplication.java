48e6f66105c7bcc19e3234e9bc0094a2b48bd5da
majian <mmdrss@163.com>
2026-06-16 11:11:46 +0800

    兼容jdk17

--- toucan-shopping-standard/apps/toucan-shopping-standard-admin-auth-web/src/main/java/com/toucan/shopping/standard/apps/admin/auth/web/app/StandardAdminAuthJarWebApplication.java
+++ toucan-shopping-standard/apps/toucan-shopping-standard-admin-auth-web/src/main/java/com/toucan/shopping/standard/apps/admin/auth/web/app/StandardAdminAuthJarWebApplication.java
@@ -10,7 +10,13 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @ComponentScan("com.toucan.shopping")
 @MapperScan({"com.toucan.shopping.modules.admin.auth.mapper",
-             "com.toucan.shopping.modules.common.persistence.event.mapper"})
+             "com.toucan.shopping.modules.common.persistence.event.mapper",
+             "com.toucan.shopping.modules.user.mapper",
+             "com.toucan.shopping.modules.area.mapper",
+             "com.toucan.shopping.modules.category.mapper",
+             "com.toucan.shopping.modules.order.mapper",
+             "com.toucan.shopping.modules.product.mapper",
+             "com.toucan.shopping.modules.stock.mapper"})
 public class StandardAdminAuthJarWebApplication {
 
     public static void main(String[] args) {
