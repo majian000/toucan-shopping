1de6c42ad2230bee155480bff69af59078260010
majian <mmdrss@163.com>
2026-06-24 10:13:52 +0800

    重构

--- toucan-shopping-cloud/modules/apps/toucan-shopping-admin-auth-web-config/src/main/java/com/toucan/shopping/cloud/apps/admin/auth/web/config/EnableFeignConfig.java
+++ toucan-shopping-cloud/modules/apps/toucan-shopping-admin-auth-web-config/src/main/java/com/toucan/shopping/cloud/apps/admin/auth/web/config/EnableFeignConfig.java
@@ -1,8 +1,10 @@
 package com.toucan.shopping.cloud.apps.admin.auth.web.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
 @EnableFeignClients(basePackages = "com.toucan.shopping")
+@Configuration
 public class EnableFeignConfig {
 
 
