eaed0aa136156b626d6b6c91b00a57ce2807a677
majian <mmdrss@163.com>
2026-06-26 15:44:08 +0800

    重构

--- toucan-shopping-cloud/modules/apps/toucan-shopping-admin-single-config/src/main/java/com/toucan/shopping/cloud/apps/admin/config/MybatisConfig.java
+++ toucan-shopping-cloud/modules/apps/toucan-shopping-admin-single-config/src/main/java/com/toucan/shopping/cloud/apps/admin/config/MybatisConfig.java
@@ -1,10 +1,11 @@
 package com.toucan.shopping.cloud.apps.admin.config;
 
 import org.mybatis.spring.annotation.MapperScan;
-
+import org.springframework.context.annotation.Configuration;
 
 
 @MapperScan({"com.toucan.shopping.modules.common.persistence.mapper","com.toucan.shopping.modules.common.persistence.event.mapper"})
+@Configuration
 public class MybatisConfig {
 
 }
