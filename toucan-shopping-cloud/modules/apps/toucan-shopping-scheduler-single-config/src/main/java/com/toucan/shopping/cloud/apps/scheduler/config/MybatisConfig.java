79ddf3ee0e3b36af7e84083bdfe558e8a4ee660b
majian <mmdrss@163.com>
2026-06-29 16:14:02 +0800

    重构

--- toucan-shopping-cloud/modules/apps/toucan-shopping-scheduler-single-config/src/main/java/com/toucan/shopping/cloud/apps/scheduler/config/MybatisConfig.java
+++ toucan-shopping-cloud/modules/apps/toucan-shopping-scheduler-single-config/src/main/java/com/toucan/shopping/cloud/apps/scheduler/config/MybatisConfig.java
@@ -4,14 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @MapperScan({"com.toucan.shopping.modules.common.persistence.mapper",
-             "com.toucan.shopping.modules.common.persistence.event.mapper",
-             "com.toucan.shopping.modules.product.mapper",
-             "com.toucan.shopping.modules.order.mapper",
-             "com.toucan.shopping.modules.stock.mapper",
-             "com.toucan.shopping.modules.content.mapper",
-             "com.toucan.shopping.modules.user.mapper",
-             "com.toucan.shopping.modules.area.mapper",
-             "com.toucan.shopping.modules.seller.mapper"})
+            "com.toucan.shopping.modules.common.persistence.event.mapper",
+            "com.toucan.shopping.modules.product.mapper",
+            "com.toucan.shopping.modules.order.mapper",
+            "com.toucan.shopping.modules.stock.mapper",
+            "com.toucan.shopping.modules.content.mapper",
+            "com.toucan.shopping.modules.user.mapper",
+            "com.toucan.shopping.modules.area.mapper",
+            "com.toucan.shopping.modules.content.mapper",
+            "com.toucan.shopping.modules.seller.mapper"})
 @Configuration
 public class MybatisConfig {
 }
--- toucan-shopping-cloud/modules/apps/toucan-shopping-seller-web-single-config/src/main/java/com/toucan/shopping/cloud/apps/seller/web/config/MybatisConfig.java
+++ toucan-shopping-cloud/modules/apps/toucan-shopping-seller-web-single-config/src/main/java/com/toucan/shopping/cloud/apps/seller/web/config/MybatisConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
         "com.toucan.shopping.modules.stock.mapper",
         "com.toucan.shopping.modules.seller.mapper",
         "com.toucan.shopping.modules.area.mapper",
+        "com.toucan.shopping.modules.content.mapper",
         "com.toucan.shopping.modules.message.mapper"})
 @Configuration
 public class MybatisConfig {
