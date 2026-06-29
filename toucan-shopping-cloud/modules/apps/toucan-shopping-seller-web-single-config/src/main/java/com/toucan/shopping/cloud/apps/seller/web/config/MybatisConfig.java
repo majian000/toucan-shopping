3fcb1519ba409dd877a65af0e095d273f941408d
majian <mmdrss@163.com>
2026-06-29 15:47:29 +0800

    重构

---
+++ toucan-shopping-cloud/modules/apps/toucan-shopping-seller-web-single-config/src/main/java/com/toucan/shopping/cloud/apps/seller/web/config/MybatisConfig.java
@@ -0,0 +1,19 @@
+package com.toucan.shopping.cloud.apps.seller.web.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@MapperScan({"com.toucan.shopping.modules.common.persistence.mapper",
+        "com.toucan.shopping.modules.common.persistence.event.mapper",
+        "com.toucan.shopping.modules.category.mapper",
+        "com.toucan.shopping.modules.order.mapper",
+        "com.toucan.shopping.modules.product.mapper",
+        "com.toucan.shopping.modules.user.mapper",
+        "com.toucan.shopping.modules.admin.auth.mapper",
+        "com.toucan.shopping.modules.stock.mapper",
+        "com.toucan.shopping.modules.message.mapper"})
+@Configuration
+public class MybatisConfig {
+
+}
