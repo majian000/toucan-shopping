267f2cab5b0e9335737c2a0bb8fbdac0bcc1e24b
majian <mmdrss@163.com>
2026-07-08 16:07:01 +0800

    修复bug

--- toucan-shopping-modules/common/toucan-shopping-common-web/src/main/java/com/toucan/shopping/modules/common/util/IPUtil.java
+++ toucan-shopping-modules/common/toucan-shopping-common-web/src/main/java/com/toucan/shopping/modules/common/util/IPUtil.java
@@ -3,6 +3,9 @@ package com.toucan.shopping.modules.common.util;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class IPUtil {
 
     /**
@@ -29,5 +32,31 @@ public class IPUtil {
             ip = request.getRemoteAddr();
         }
         return ip;
+    }
+
+
+    /**
+     * 输入cidr格式 查看是否能匹配IP
+     * @param ip
+     * @param cidr 10.0.0.0/8
+     * @return
+     * @throws UnknownHostException
+     */
+    public static boolean matches(String ip, String cidr) throws UnknownHostException {
+        String[] parts = cidr.split("/");
+        byte[] ipBytes = InetAddress.getByName(ip).getAddress();
+        byte[] netBytes = InetAddress.getByName(parts[0]).getAddress();
+        int mask = Integer.parseInt(parts[1]);
+
+        int bits = ipBytes.length * 8;  // IPv4=32, IPv6=128
+        for (int i = 0; i < bits; i++) {
+            int byteIdx = i / 8;
+            int bitIdx = 7 - (i % 8);      // 从高位开始
+            int ipBit = (ipBytes[byteIdx] >> bitIdx) & 1;
+            int netBit = (netBytes[byteIdx] >> bitIdx) & 1;
+            if (i < mask && ipBit != netBit) return false;
+            if (i >= mask) break;           // 掩码之后的部分不比较
+        }
+        return true;
     }
 }
