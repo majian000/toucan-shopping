d1515021eb9577694fe662cc039472d9c93293c2
majian <mmdrss@163.com>
2026-07-25 16:33:16 +0800

    完善分布式锁

--- toucan-shopping-modules/common/toucan-shopping-lock-redis/src/test/java/com/toucan/shopping/modules/common/lock/redis/TestRedisConfig.java
+++ toucan-shopping-modules/common/toucan-shopping-lock-redis/src/test/java/com/toucan/shopping/modules/common/lock/redis/TestRedisConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * 集成测试配置 —— 使用application.properties中的Redis地址
  */
-@SpringBootConfiguration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.toucan.shopping.modules.common.lock.redis")
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages = "com.toucan.shopping.modules.common.lock.redis")
 public class TestRedisConfig {
 }
