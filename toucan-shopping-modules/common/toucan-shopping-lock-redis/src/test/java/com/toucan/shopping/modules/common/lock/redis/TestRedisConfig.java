278925556fa346276ca526574ad4fa66b217a4cf
majian <mmdrss@163.com>
2026-07-25 13:41:13 +0800

    接口监控

--- toucan-shopping-modules/common/toucan-shopping-lock-redis/src/test/java/com/toucan/shopping/modules/common/lock/redis/TestRedisConfig.java
+++ toucan-shopping-modules/common/toucan-shopping-lock-redis/src/test/java/com/toucan/shopping/modules/common/lock/redis/TestRedisConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * 集成测试配置 —— 使用application.properties中的Redis地址(8.140.187.184:6379)
  */
-@SpringBootConfiguration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.toucan.shopping.modules.common.lock.redis")
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages = "com.toucan.shopping.modules.common.lock.redis")
 public class TestRedisConfig {
 }
