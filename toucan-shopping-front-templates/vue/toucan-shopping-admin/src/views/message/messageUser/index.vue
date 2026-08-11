6545d24fb53c8aa5dd81a49189dc18640800109e
majian <mmdrss@163.com>
2026-08-11 16:40:00 +0800

    功能开发

--- toucan-shopping-front-templates/vue/toucan-shopping-admin/src/views/message/messageUser/index.vue
+++ toucan-shopping-front-templates/vue/toucan-shopping-admin/src/views/message/messageUser/index.vue
@@ -46,7 +46,7 @@
         <el-table-column prop="userMainId" label="用户ID" width="180" show-overflow-tooltip />
         <el-table-column label="状态" width="80" align="center">
           <template #default="{ row }">
-            <el-tag :type="row.status === '0' ? 'warning' : 'success'" size="small">
+            <el-tag :type="row.status === 0 ? 'info' : 'success'" size="small">
               {{ row.status === 0 ? '未读' : '已读' }}
             </el-tag>
           </template>
