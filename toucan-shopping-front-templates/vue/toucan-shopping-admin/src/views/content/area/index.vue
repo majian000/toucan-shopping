bc9481bda921d7f5541d481386f04d0ce671d3a6
majian <mmdrss@163.com>
2026-08-12 17:26:12 +0800

    feat:移植功能

--- toucan-shopping-front-templates/vue/toucan-shopping-admin/src/views/content/area/index.vue
+++ toucan-shopping-front-templates/vue/toucan-shopping-admin/src/views/content/area/index.vue
@@ -66,29 +66,50 @@
             :load="loadParentAreaTree" lazy node-key="id"
           />
         </el-form-item>
-        <el-form-item label="名称" prop="name">
-          <el-input v-model="formData.name" placeholder="请输入名称" maxlength="100" />
+        <el-form-item label="国家">
+          <el-select v-model="formData.countryCode" style="width:100%">
+            <el-option label="中国" value="CHN" />
+          </el-select>
         </el-form-item>
-        <el-form-item label="编码" prop="code">
-          <el-input v-model="formData.code" placeholder="请输入编码" maxlength="50" />
+        <el-form-item label="大区">
+          <el-select v-model="formData.bigAreaCode" style="width:100%">
+            <el-option label="华东" value="HUADONG" />
+            <el-option label="华北" value="HUABEI" />
+            <el-option label="华中" value="HUAZHONG" />
+            <el-option label="华南" value="HUANAN" />
+            <el-option label="东北" value="DONGBEI" />
+            <el-option label="西北" value="XIBEI" />
+            <el-option label="西南" value="XINAN" />
+            <el-option label="港澳台" value="GANGAOTAI" />
+          </el-select>
         </el-form-item>
         <el-form-item label="类型" prop="type">
-          <el-input v-model="formData.type" placeholder="请输入类型" maxlength="20" />
+          <el-radio-group v-model="formData.type">
+            <el-radio :value="1">省</el-radio>
+            <el-radio :value="2">市</el-radio>
+            <el-radio :value="3">区县</el-radio>
+          </el-radio-group>
         </el-form-item>
-        <el-form-item label="排序">
+        <el-form-item v-if="formData.type === 1" label="省名称" prop="province">
+          <el-input v-model="formData.province" placeholder="请输入省名称" maxlength="20" />
+        </el-form-item>
+        <el-form-item v-if="formData.type === 2" label="市名称" prop="city">
+          <el-input v-model="formData.city" placeholder="请输入市名称" maxlength="20" />
+        </el-form-item>
+        <el-form-item v-if="formData.type === 3" label="区县名称" prop="area">
+          <el-input v-model="formData.area" placeholder="请输入区县名称" maxlength="20" />
+        </el-form-item>
+        <el-form-item label="编码" prop="code">
+          <el-input v-model="formData.code" placeholder="请输入编码" maxlength="20" />
+        </el-form-item>
+        <el-form-item label="排序" prop="areaSort">
           <el-input-number v-model="formData.areaSort" :min="0" style="width:100%" />
         </el-form-item>
         <el-form-item label="是否直辖市">
           <el-switch v-model="formData.isMunicipality" :active-value="1" :inactive-value="0" />
         </el-form-item>
-        <el-form-item label="国家编码">
-          <el-input v-model="formData.countryCode" placeholder="请输入国家编码" maxlength="50" />
-        </el-form-item>
-        <el-form-item label="大区编码">
-          <el-input v-model="formData.bigAreaCode" placeholder="请输入大区编码" maxlength="50" />
-        </el-form-item>
-        <el-form-item label="父编码">
-          <el-input v-model="formData.parentCode" placeholder="请输入父编码" maxlength="50" />
+        <el-form-item label="备注">
+          <el-input v-model="formData.remark" type="textarea" :rows="2" maxlength="255" placeholder="请输入备注信息" />
         </el-form-item>
       </el-form>
       <template #footer>
@@ -132,16 +153,21 @@ const submitLoading = ref(false); const formRef = ref(null)
 const parentAreaTree = ref([])
 const dialogTitle = computed(() => isEdit.value ? '编辑地区' : '添加地区')
 
-const formData = reactive({ id: null, pid: null, name: '', code: '', type: '', areaSort: 0, isMunicipality: 0, countryCode: '', bigAreaCode: '', parentCode: '' })
+const formData = reactive({ id: null, pid: null, name: '', code: '', type: 1, province: '', city: '', area: '', areaSort: 0, isMunicipality: 0, countryCode: 'CHN', bigAreaCode: 'HUADONG', parentCode: '', remark: '' })
 const formRules = {
   name: [{ required: true, message: '请输入名称', trigger: 'blur' }],
   code: [{ required: true, message: '请输入编码', trigger: 'blur' }],
-  type: [{ required: true, message: '请输入类型', trigger: 'blur' }]
+  type: [{ required: true, message: '请选择类型', trigger: 'change' }],
+  province: [{ required: true, message: '请输入省名称', trigger: 'blur' }],
+  city: [{ required: true, message: '请输入市名称', trigger: 'blur' }],
+  area: [{ required: true, message: '请输入区县名称', trigger: 'blur' }]
 }
 
 function resetForm() {
-  formData.id = null; formData.pid = null; formData.name = ''; formData.code = ''; formData.type = ''
-  formData.areaSort = 0; formData.isMunicipality = 0; formData.countryCode = ''; formData.bigAreaCode = ''; formData.parentCode = ''
+  formData.id = null; formData.pid = null; formData.name = ''; formData.code = ''; formData.type = 1
+  formData.province = ''; formData.city = ''; formData.area = ''
+  formData.areaSort = 0; formData.isMunicipality = 0; formData.countryCode = 'CHN'; formData.bigAreaCode = 'HUADONG'
+  formData.parentCode = ''; formData.remark = ''
 }
 
 async function loadParentAreaRoots() {
@@ -162,9 +188,11 @@ async function openDialog(title, row) {
   if (row) {
     isEdit.value = true
     formData.id = row.id; formData.pid = row.pid; formData.name = row.name || ''; formData.code = row.code || ''
-    formData.type = row.type || ''; formData.areaSort = row.areaSort || 0
-    formData.isMunicipality = row.isMunicipality || 0; formData.countryCode = row.countryCode || ''
-    formData.bigAreaCode = row.bigAreaCode || ''; formData.parentCode = row.parentCode || ''
+    formData.type = row.type != null ? row.type : 1; formData.province = row.province || ''; formData.city = row.city || ''
+    formData.area = row.area || ''; formData.areaSort = row.areaSort || 0
+    formData.isMunicipality = row.isMunicipality || 0; formData.countryCode = row.countryCode || 'CHN'
+    formData.bigAreaCode = row.bigAreaCode || 'HUADONG'; formData.parentCode = row.parentCode || ''
+    formData.remark = row.remark || ''
   } else {
     isEdit.value = false
     resetForm()
