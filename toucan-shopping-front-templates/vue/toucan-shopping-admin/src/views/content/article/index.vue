c21c1e17eaa286a8e378f7590efaf0222b767734
majian <mmdrss@163.com>
2026-08-14 16:19:04 +0800

    feat:移植功能

--- toucan-shopping-front-templates/vue/toucan-shopping-admin/src/views/content/article/index.vue
+++ toucan-shopping-front-templates/vue/toucan-shopping-admin/src/views/content/article/index.vue
@@ -71,8 +71,8 @@
 
         <el-card shadow="never" class="table-card">
           <div class="toolbar">
-            <el-button type="primary" :icon="Plus" @click="handleAdd">添加文章</el-button>
-            <el-button type="danger" :icon="Delete" :disabled="selectedRows.length === 0" @click="handleBatchDelete">批量删除</el-button>
+            <el-button type="primary" :icon="Plus" v-permission="'toucan:content:article:add'" @click="handleAdd">添加文章</el-button>
+            <el-button type="danger" :icon="Delete" v-permission="'toucan:content:article:deletes'" :disabled="selectedRows.length === 0" @click="handleBatchDelete">批量删除</el-button>
           </div>
           <el-table
             ref="tableRef"
@@ -115,9 +115,9 @@
             <el-table-column prop="updateDate" label="修改时间" width="170" />
             <el-table-column label="操作" width="200" fixed="right" align="center">
               <template #default="{ row }">
-                <el-button type="info" link size="small" :icon="View" @click="handleView(row)">查看</el-button>
-                <el-button type="primary" link size="small" :icon="Edit" @click="handleEdit(row)">编辑</el-button>
-                <el-button type="danger" link size="small" :icon="Delete" @click="handleDelete(row)">删除</el-button>
+                <el-button type="info" link size="small" :icon="View" v-permission="'toucan:content:article:detail'" @click="handleView(row)">查看</el-button>
+                <el-button type="primary" link size="small" :icon="Edit" v-permission="'toucan:content:article:update'" @click="handleEdit(row)">编辑</el-button>
+                <el-button type="danger" link size="small" :icon="Delete" v-permission="'toucan:content:article:delete'" @click="handleDelete(row)">删除</el-button>
               </template>
             </el-table-column>
           </el-table>
