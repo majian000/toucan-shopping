c0a40720498d4baa628348ccda96e49a954bb757
majian <mmdrss@163.com>
2026-08-17 16:32:08 +0800

    feat:移植功能

--- toucan-shopping-front-templates/vue/toucan-shopping-admin/src/api/product/hotSellStatistic.js
+++ toucan-shopping-front-templates/vue/toucan-shopping-admin/src/api/product/hotSellStatistic.js
@@ -1,6 +1,7 @@
 import request from '@/utils/request'
 
 // 查询热销列表 (OrderHotSellPageInfo @RequestBody, JSON body)
+// 该接口为聚合统计查询、后端较慢，单独调大超时时间(5分钟)
 export function queryHotSellListPage(data) {
-  return request({ url: '/productSkuStatistic/queryHotSellListPage', method: 'post', data })
+  return request({ url: '/productSkuStatistic/queryHotSellListPage', method: 'post', data, timeout: 300000 })
 }
--- toucan-shopping-front-templates/vue/toucan-shopping-admin/src/api/product/skuStatistic.js
+++ toucan-shopping-front-templates/vue/toucan-shopping-admin/src/api/product/skuStatistic.js
@@ -1,8 +1,9 @@
 import request from '@/utils/request'
 
 // 分类下商品统计 (ProductSkuStatisticVO @RequestBody -> data)
+// 该接口为聚合统计查询、后端较慢，单独调大超时时间(5分钟)
 export function queryProductSkuStatistic(data) {
-  return request({ url: '/productSkuStatistic/queryProductSkuStatistic', method: 'post', data })
+  return request({ url: '/productSkuStatistic/queryProductSkuStatistic', method: 'post', data, timeout: 300000 })
 }
 
 // 分类树 (CategoryVO @RequestBody -> data)
